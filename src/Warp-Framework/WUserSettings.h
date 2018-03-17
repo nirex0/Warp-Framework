c068f58b7df7a71086fb14fd22e432439d5e92c1
nirex0 <nirex0@users.noreply.github.com>
2018-03-17 14:36:44 +0330

    Add: (Framework) FRAMEWORK_VERSION constant

--- src/Warp-Framework/WUserSettings.h
+++ src/Warp-Framework/WUserSettings.h
@@ -5,10 +5,11 @@
 
 #include "WWin.h"
 
-LPWSTR ApplicationName = L"DX_APP_NAME";
-LPWSTR WindowTitle = L"DirectX App";
+static constexpr LPWSTR ApplicationName = L"DX_APP_NAME";
+static constexpr LPWSTR WindowTitle = L"DirectX App";
 
 #define W_WIDTH 800
 #define W_HEIGHT 600
+#define FRAMEWORK_VERSION 1
 
 #endif // _W_USER_SETTINGS_H_
