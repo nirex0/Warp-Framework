8d59d1809d0328977e86a69ae61a6e0c4ea20f66
nirex0 <nirex0@users.noreply.github.com>
2018-03-17 21:08:55 +0330

    Fix: (Memory) Memory management is now done properly

--- src/Warp-Framework/WEntry.h
+++ src/Warp-Framework/WEntry.h
@@ -13,9 +13,14 @@ class WEntry final : public WEntity
 {
 public:
 	inline WEntry() : m_mouse(nullptr), m_keyboard(nullptr) {}
+	inline ~WEntry()
+	{
+		delete m_mouse;
+		delete m_keyboard;
+	}
+
 	void Start(void);
 	void Update(double deltaTime);
-
 
 	inline WMouse* Mouse(void) { return this->m_mouse; }
 	inline WEntry* Mouse(WMouse* const intake) { this->m_mouse = intake; return this; };
