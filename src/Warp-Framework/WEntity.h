b772f570ce256b87df5e42d92fddba52a26fe411
nirex0 <nirex0@users.noreply.github.com>
2018-03-22 21:51:17 +0430

    Remove: (Comment) Removed a useless comment

--- src/Warp-Framework/WEntity.h
+++ src/Warp-Framework/WEntity.h
@@ -1,9 +1,8 @@
-//Â© 2018 NIREX ALL RIGHTS RESERVED
+//© 2018 NIREX ALL RIGHTS RESERVED
 
 #ifndef _W_ENTITY_H_
 #define _W_ENTITY_H_
 
-// The base class for almost all classes in the framework
 class WEntity
 {
 };
