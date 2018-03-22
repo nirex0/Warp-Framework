0fd25c1ace77a6787d58d5920232035791c059cb
nirex0 <nirex0@users.noreply.github.com>
2018-03-22 21:49:53 +0430

    Add: (Framework) Saferelease
    This is a required Template function for all COM objects.

---
+++ src/Warp-Framework/WSafeRelease.h
@@ -0,0 +1,15 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_SAFE_RELEASE_H_
+#define _W_SAFE_RELEASE_H_
+
+template <class T> void SafeRelease(T **ppT)
+{
+    if (*ppT)
+    {
+        (*ppT)->Release();
+        *ppT = NULL;
+    }
+}
+
+#endif // !_W_SAFE_RELEASE_H_
