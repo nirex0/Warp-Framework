95cabddb7ec176b5f12974b7ddc7aa2c4e481620
nirex0 <nirex0@users.noreply.github.com>
2018-03-23 03:12:32 +0430

    Add: (WWin) Added more #defines to shorten the compile time

--- src/Warp-Framework/WWin.h
+++ src/Warp-Framework/WWin.h
@@ -5,7 +5,42 @@
 
 #ifndef _STD_WINDOWS_H_
 #define _STD_WINDOWS_H_
+
 #define WIN32_LEAN_AND_MEAN
+#define NOGDICAPMASKS
+//#define NOSYSMETRICS
+#define NOMENUS
+#define NOICONS
+#define NOSYSCOMMANDS
+#define NORASTEROPS
+#define OEMRESOURCE
+#define NOATOM
+#define NOCLIPBOARD
+#define NOCOLOR
+#define NOCTLMGR
+#define NODRAWTEXT
+#define NOKERNEL
+#define NONLS
+#define NOMEMMGR
+#define NOMETAFILE
+#define NOMINMAX
+#define NOOPENFILE
+#define NOSCROLL
+#define NOSERVICE
+#define NOSOUND
+#define NOTEXTMETRIC
+#define NOWH
+#define NOCOMM
+#define NOKANJI
+#define NOHELP
+#define NOPROFILER
+#define NODEFERWINDOWPOS
+#define NOMCX
+#define NORPC
+#define NOPROXYSTUB
+#define NOIMAGE
+#define NOTAPE
+
 #include <Windows.h>
 #endif // !_STD_WINDOWS_H_
 
