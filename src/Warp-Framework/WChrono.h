203bcd800617d5cf2f8ed56528e63661b01ff93f
Nirex0 <Nirex.0@gmail.com>
2018-05-07 22:34:57 +0430

    Update: Chrono now has its own header file

---
+++ src/Warp-Framework/WChrono.h
@@ -0,0 +1,16 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_CHRONO_H_
+#define _W_CHRONO_H_
+
+#include "WDefines.h"
+
+#include <chrono>
+
+typedef std::chrono::duration<long, std::ratio<1, 60>> sixtieths_of_a_sec;
+constexpr auto kMaxDeltatime = sixtieths_of_a_sec{ 1 };
+using WClock = std::chrono::steady_clock;
+auto mLastEndTime = WClock::now();
+DELTATIME milliseconds = 0.0;
+
+#endif // !_W_CHRONO_H_
