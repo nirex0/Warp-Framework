d22585a58ec3f9500831ed00bb158912eff49473
Nirex0 <Nirex.0@gmail.com>
2018-05-18 20:59:41 +0430

    Update: Improvement regarding colors

--- src/Warp-Framework/WContainer.h
+++ src/Warp-Framework/WContainer.h
@@ -33,17 +33,17 @@ public:
 	static const DELTATIME DeltaSeconds(DELTATIME seconds) { WContainer::DeltaTime = seconds; return WContainer::DeltaTime; }
 	static const DELTATIME DeltaSeconds(void) { return WContainer::DeltaTime; }
 
-	static const W_BYTE BackA(W_BYTE intake) { WContainer::BGA = intake; return WContainer::BGA; }
-	static const W_BYTE BackA(void) { return WContainer::BGA; }
+	static const W_INT BackA(W_INT intake) { WContainer::BGA = intake; return WContainer::BGA; }
+	static const W_INT BackA(void) { return WContainer::BGA; }
 
-	static const W_BYTE BackR(W_BYTE intake) { WContainer::BGR = intake; return WContainer::BGR; }
-	static const W_BYTE BackR(void) { return WContainer::BGR; }
+	static const W_INT BackR(W_INT intake) { WContainer::BGR = intake; return WContainer::BGR; }
+	static const W_INT BackR(void) { return WContainer::BGR; }
 
-	static const W_BYTE BackG(W_BYTE intake) { WContainer::BGG = intake; return WContainer::BGG; }
-	static const W_BYTE BackG(void) { return WContainer::BGG; }
+	static const W_INT BackG(W_INT intake) { WContainer::BGG = intake; return WContainer::BGG; }
+	static const W_INT BackG(void) { return WContainer::BGG; }
 
-	static const W_BYTE BackB(W_BYTE intake) { WContainer::BGB = intake; return WContainer::BGB; }
-	static const W_BYTE BackB(void) { return WContainer::BGB; }
+	static const W_INT BackB(W_INT intake) { WContainer::BGB = intake; return WContainer::BGB; }
+	static const W_INT BackB(void) { return WContainer::BGB; }
 
 	static const W_INT Width(W_INT intake) { WContainer::W_WIDTH = intake; return WContainer::W_WIDTH; }
 	static const W_INT Width(void) { return WContainer::W_WIDTH; }
@@ -65,10 +65,10 @@ private:
 	static LPARAM WContainer::lParam;
 	static WEntry WContainer::WFramework;
 	static DELTATIME WContainer::DeltaTime;
-	static W_BYTE WContainer::BGA;
-	static W_BYTE WContainer::BGR;
-	static W_BYTE WContainer::BGG;
-	static W_BYTE WContainer::BGB;
+	static W_INT WContainer::BGA;
+	static W_INT WContainer::BGR;
+	static W_INT WContainer::BGG;
+	static W_INT WContainer::BGB;
 	static W_INT WContainer::W_WIDTH;
 	static W_INT WContainer::W_HEIGHT;
 	static WMouse* WContainer::mouse;
