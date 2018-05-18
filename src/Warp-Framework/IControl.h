a678ba68b645ed5365d35c04ff7cb3c014778dd2
Nirex0 <Nirex.0@gmail.com>
2018-05-18 21:00:42 +0430

    Add: ZIndex, AutoRender and support for mouse roll events

--- src/Warp-Framework/IControl.h
+++ src/Warp-Framework/IControl.h
@@ -32,26 +32,43 @@ public:
 	virtual WRegistry* MouseUpRegistery(void) = 0;
 	virtual WRegistry* MouseEnterRegistery(void) = 0;
 	virtual WRegistry* MouseLeaveRegistery(void) = 0;
+	virtual WRegistry* MouseRollUpRegistery(void) = 0;
+	virtual WRegistry* MouseRollDownRegistery(void) = 0;
 
 	// Setters
 	virtual WRegistry* MouseDownRegistery(WRegistry* intake) = 0;
 	virtual WRegistry* MouseUpRegistery(WRegistry* intake) = 0;
 	virtual WRegistry* MouseEnterRegistery(WRegistry* intake) = 0;
 	virtual WRegistry* MouseLeaveRegistery(WRegistry* intake) = 0;
+	virtual WRegistry* MouseRollUpRegistery(WRegistry* intake) = 0;
+	virtual WRegistry* MouseRollDownRegistery(WRegistry* intake) = 0;
 
 	// Getters
+	virtual int ZIndex(void) const = 0;
 	virtual bool IsEnabled(void) const = 0;
 	virtual bool IsVisible(void) const = 0;
+	virtual bool AutoRender(void) const = 0;
 
 	// Setters
+	virtual int ZIndex(int input) = 0;
 	virtual bool IsEnabled(bool input) = 0;
 	virtual bool IsVisible(bool input) = 0;
+	virtual bool AutoRender(bool input) = 0;
 
 	// Events
 	virtual void MouseDown(WMouseArgs* Args) = 0;
 	virtual void MouseUp(WMouseArgs* Args) = 0;
 	virtual void MouseEnter(WMouseArgs* Args) = 0;
 	virtual void MouseLeave(WMouseArgs* Args) = 0;
+	virtual void MouseRollUp(WMouseArgs* Args) = 0;
+	virtual void MouseRollDown(WMouseArgs* Args) = 0;
+
+	// Helpers
+	virtual bool IsWithin(WMouseArgs* Args) const = 0;
+	virtual void SetZIndexNoChange(int zIndex) = 0;
+
+	// Visual
+	virtual void Render(void) = 0;
 };
 
 #endif // !_I_CONTROL_H_
