7d01d3b3827d4573a067a897d2ef77312fbde54a
Nirex0 <Nirex.0@gmail.com>
2018-05-18 20:58:28 +0430

    Add: ZIndex for Controls

--- src/Warp-Framework/WEntry.cpp
+++ src/Warp-Framework/WEntry.cpp
@@ -1,14 +1,17 @@
 //© 2018 NIREX ALL RIGHTS RESERVED
 
 #include "WContainer.h"
+#include "WControlHandler.h"
+
 
 // WButtonHandler
-std::vector<IControl*> WControlHandler::vtcp = {};
+std::map<int, IControl*> WControlHandler::mtcp = {};
+std::set<int> WControlHandler::stcz = {};
 
 //Runs at the start of the application
 void WEntry::Start(void)
 {
-
+	
 }
 
 //Runs every frame pre-render
@@ -20,5 +23,6 @@ void WEntry::Update(DELTATIME deltaTime)
 //Runs every frame post-update
 void WEntry::Render(DELTATIME deltaTime)
 {
-
+	
 }
+
