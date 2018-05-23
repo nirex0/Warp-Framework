062a7c68ade23d2d6ada846d250f8f2b52d167dd
Nirex0 <Nirex.0@gmail.com>
2018-05-23 22:59:09 +0430

    Update: WAudio is no longer a child of WEntity

--- src/Warp-Framework/WAudio.h
+++ src/Warp-Framework/WAudio.h
@@ -3,13 +3,12 @@
 #ifndef _W_AUDIO_H_
 #define _W_AUDIO_H_
 
-#include "WEntity.h"
 #include "WWin.h"
 #include "WMci.h"
 #include "WContainer.h"
 #include <string>
 
-class WAudio : public WEntity
+class WAudio
 {
 public:
 	WAudio(const wchar_t* name, const wchar_t* path);
@@ -26,11 +25,9 @@ public:
 	void Stop(void);
 	void PlayOnRepeat(void);
 
-
 private:
 	const wchar_t* m_name;
 	const wchar_t* m_path;
-
 };
 
 #endif // !_W_AUDIO_H_
