6c0fbf470b99ef188d80fac311106593c80064ef
Nirex0 <Nirex.0@gmail.com>
2018-05-23 22:51:52 +0430

    Add: Header for Audio support

---
+++ src/Warp-Framework/WMci.h
@@ -0,0 +1,11 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_MCI_H_
+#define _W_MCI_H_
+
+#include <Mmsystem.h>
+#include <mciapi.h>
+//these two headers are already included in the <Windows.h> header
+#pragma comment(lib, "Winmm.lib")
+
+#endif // !_W_MCI_H_
