90a83f78fc3e2492860dd543485cc0f646c83844
Nirex0 <Nirex.0@gmail.com>
2018-06-17 10:10:29 +0430

    Add: GFXContainer now holds RenderMethod

--- src/Warp-Framework/WGFXContainer.h
+++ src/Warp-Framework/WGFXContainer.h
@@ -11,8 +11,12 @@ public:
 	static WGraphics* Graphics(void) { return WGraphicsContainer::gfx; }
 	static WGraphics* Graphics(WGraphics* intake) { WGraphicsContainer::gfx = intake; return WGraphicsContainer::gfx; }
 
+	static D2D1_RENDER_TARGET_TYPE RenderMethod(void) { return WGraphicsContainer::main_rttype; }
+	static D2D1_RENDER_TARGET_TYPE RenderMethod(D2D1_RENDER_TARGET_TYPE intake) { WGraphicsContainer::main_rttype = intake; return WGraphicsContainer::main_rttype; }
+
 private:
 	static WGraphics* WGraphicsContainer::gfx;
+	static WGraphicsContainer::D2D1_RENDER_TARGET_TYPE main_rttype;
 };
 
 
