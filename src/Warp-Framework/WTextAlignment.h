c6d03221435fc3efd8914cb205a6946c543902bc
Nirex0 <Nirex.0@gmail.com>
2018-06-22 20:48:23 +0430

    Add: Text alignment enum

---
+++ src/Warp-Framework/WTextAlignment.h
@@ -0,0 +1,13 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_TEXT_ALIGNMENT_H_
+#define _W_TEXT_ALIGNMENT_H_
+
+enum WTextAlignment
+{
+	WTA_RightToLeft,
+	WTA_Center,
+	WTA_LeftToRight
+};
+
+#endif // !_W_TEXT_ALIGNMENT_H_
