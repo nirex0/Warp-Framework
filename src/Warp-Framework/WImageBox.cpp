5d4ef7e9cfd8bf7cfc34ec0485efe7292162c2a0
Nirex0 <Nirex.0@gmail.com>
2018-07-06 17:54:51 +0430

    Add: New WImageBox control

---
+++ src/Warp-Framework/WImageBox.cpp
@@ -0,0 +1,121 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WImageBox.h"
+#include "WControlHandler.h"
+#include "WSafeRelease.h"
+
+WImageBox::WImageBox(wchar_t* URL, W_INT zIndex)
+	: WControl(zIndex)
+	, m_URL(URL)
+	, opacity(1.0F)
+{
+	WGraphicsContainer::Graphics()->LoadIMG(URL, &img);
+}
+
+WImageBox::WImageBox(wchar_t* URL, W_FLOAT top, W_FLOAT left, W_FLOAT bottom, W_FLOAT right, W_INT zIndex)
+	: WControl(top, left, bottom, right, zIndex)
+	, m_URL(URL)
+	, opacity(1.0F)
+{
+	WGraphicsContainer::Graphics()->LoadIMG(URL, &img);
+}
+
+WImageBox::WImageBox(wchar_t* URL, WPointF topleft, WPointF botright, W_INT zIndex)
+	: WControl(topleft, botright, zIndex)
+	, m_URL(URL)
+	, opacity(1.0F)
+{
+	WGraphicsContainer::Graphics()->LoadIMG(URL, &img);
+}
+
+WImageBox::WImageBox(wchar_t* URL, WRectF location, W_INT zIndex)
+	: WControl(location, zIndex)
+	, m_URL(URL)
+	, opacity(1.0F)
+{
+	WGraphicsContainer::Graphics()->LoadIMG(URL, &img);
+}
+
+WImageBox::~WImageBox(void)
+{
+	delete img;
+}
+
+void WImageBox::Render(void)
+{
+	if (!m_isVisible)
+		return;
+
+	ID2D1Layer* maskLayer;
+	D2D_RECT_F ParentRect;
+	if (m_Parent)
+	{
+		if (!m_Parent->IsEnabled())
+			return;
+		if (!m_Parent->IsVisible())
+			return;
+
+		ParentRect.top = m_Parent->Location().Top();
+		ParentRect.left = m_Parent->Location().Left();
+		ParentRect.bottom = m_Parent->Location().Bottom();
+		ParentRect.right = m_Parent->Location().Right();
+	}
+	else
+	{
+		ParentRect.top = 0.0F;
+		ParentRect.left = 0.0F;
+		ParentRect.bottom = (W_FLOAT)INFINITE;
+		ParentRect.right = (W_FLOAT)INFINITE;
+	}
+
+// Mask
+	ID2D1PathGeometry* MaskGeo;
+	WGraphicsContainer::Graphics()->GetFactory()->CreatePathGeometry(&MaskGeo);
+
+// Geometry Sink
+	ID2D1GeometrySink* pSink = NULL;
+	MaskGeo->Open(&pSink);
+	pSink->SetFillMode(D2D1_FILL_MODE_WINDING);
+	pSink->BeginFigure(D2D1::Point2F(ParentRect.left, ParentRect.top), D2D1_FIGURE_BEGIN_FILLED);
+	pSink->AddLine(D2D1::Point2F(ParentRect.right, ParentRect.top));
+	pSink->AddLine(D2D1::Point2F(ParentRect.right, ParentRect.bottom));
+	pSink->AddLine(D2D1::Point2F(ParentRect.left, ParentRect.bottom));
+	pSink->EndFigure(D2D1_FIGURE_END_CLOSED);
+	pSink->Close();
+
+// Begin Mask Render
+	WGraphicsContainer::Graphics()->GetRenderTarget()->CreateLayer(NULL, &maskLayer);
+	WGraphicsContainer::Graphics()->GetRenderTarget()->PushLayer(D2D1::LayerParameters(D2D1::InfiniteRect(), MaskGeo), maskLayer);
+
+// Render Statements Go Here
+	WGraphicsContainer::Graphics()->DrawIMG(img, ctRec, opacity);
+
+// End Mask Render
+	WGraphicsContainer::Graphics()->GetRenderTarget()->PopLayer();
+	SafeRelease(&maskLayer);
+	SafeRelease(&MaskGeo);
+	SafeRelease(&pSink);
+}
+
+wchar_t* WImageBox::URL(void) const
+{
+	return m_URL;
+}
+
+wchar_t* WImageBox::URL(wchar_t* intake)
+{
+	m_URL = intake;
+	WGraphicsContainer::Graphics()->LoadIMG(intake, &img);
+	return m_URL;
+}
+
+W_FLOAT WImageBox::Opacity(void) const
+{
+	return opacity;
+}
+
+W_FLOAT WImageBox::Opacity(W_FLOAT intake)
+{
+	opacity = intake;
+	return opacity;
+}
---
+++ src/Warp-Framework/WImageBox.h
@@ -0,0 +1,33 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_IMAGE_BOX_H_
+#define _W_IMAGE_BOX_H_
+
+#include "WControl.h"
+#include "WLerp.h"
+
+class WImageBox : public WControl
+{
+public:
+	WImageBox(wchar_t* URL, W_INT zIndex = 0);
+	WImageBox(wchar_t* URL, W_FLOAT top, W_FLOAT left, W_FLOAT bottom, W_FLOAT right, W_INT zIndex = 0);
+	WImageBox(wchar_t* URL, WPointF topleft, WPointF botright, W_INT zIndex = 0);
+	WImageBox(wchar_t* URL, WRectF location, W_INT zIndex = 0);
+	~WImageBox(void);
+
+// Functions
+	void Render(void) override;
+
+	wchar_t* URL(void) const;
+	wchar_t* URL(wchar_t* intake);
+
+	W_FLOAT Opacity(void) const;
+	W_FLOAT Opacity(W_FLOAT intake);
+
+private:
+	W_FLOAT opacity;
+	W_IMAGE* img;
+	wchar_t* m_URL;
+};
+
+#endif // !_W_IMAGE_BOX_H_
