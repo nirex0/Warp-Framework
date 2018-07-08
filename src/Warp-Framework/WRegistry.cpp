bd4c2e970839b61a01273bdd52e45dbcc564535f
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:33:40 +0430

    Change: Seperated the definition from declaration of WRegistry

---
+++ src/Warp-Framework/WRegistry.cpp
@@ -0,0 +1,118 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WRegistry.h"
+
+WRegistry::WRegistry()
+	: m_count(0)
+{
+}
+
+WRegistry::WRegistry(const WRegistry& nRegistery)
+	: m_count(0)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+	for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
+	{
+		this->Register(*nRegistery.m_registered[i]);
+		_InterlockedIncrement(&m_count);
+	}
+}
+
+WRegistry::WRegistry(WRegistry&& nRegistery)
+	: m_count(0)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+	for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
+	{
+
+		this->Register(*nRegistery.m_registered[i]);
+		_InterlockedIncrement(&m_count);
+	}
+}
+
+WRegistry::~WRegistry()
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+}
+
+long WRegistry::GetCount(void) const
+{
+	return m_count;
+}
+
+WRegistry* WRegistry::operator()(WEntity* sender, WEventArgs* args)
+{
+	Annex current;
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] != nullptr)
+		{
+			current = *m_registered[i];
+			current(sender, args);
+		}
+	}
+	return this;
+}
+
+WRegistry* WRegistry::Run(WEntity* sender, WEventArgs* args)
+{
+	Annex current;
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] != nullptr)
+		{
+			current = *m_registered[i];
+			current(sender, args);
+		}
+	}
+	return this;
+}
+
+WRegistry* WRegistry::Register(Annex rhs)
+{
+	m_registered.push_back(new Annex(rhs));
+	_InterlockedIncrement(&m_count);
+	return this;
+}
+
+WRegistry* WRegistry::UnRegister(Annex* rhs)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] == rhs)
+		{
+			m_registered[i] = nullptr;
+			_InterlockedDecrement(&m_count);
+		}
+	}
+	return this;
+}
+
+WRegistry* WRegistry::operator+=(Annex rhs)
+{
+	m_registered.push_back(new Annex(rhs));
+	_InterlockedIncrement(&m_count);
+	return this;
+}
+
+WRegistry* WRegistry::operator-=(Annex rhs)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] == &rhs)
+		{
+			m_registered[i] = nullptr;
+			_InterlockedDecrement(&m_count);
+		}
+	}
+	return this;
+}
--- src/Warp-Framework/WRegistry.h
+++ src/Warp-Framework/WRegistry.h
@@ -14,120 +14,19 @@ typedef std::function<void(WEntity* sender, WEventArgs* args)> Annex;
 class WRegistry final : public WEntity
 {
 public:
-	WRegistry() 
-		: m_count(0)
-	{
-	}
+	WRegistry();
+	WRegistry(const WRegistry& nRegistery);
+	WRegistry(WRegistry&& nRegistery);
+	~WRegistry();
 	
-	WRegistry(const WRegistry& nRegistery)
-		: m_count(0)
-	{
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			delete m_registered[i];
-		}
-		for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
-		{
-			this->Register(*nRegistery.m_registered[i]);
-			_InterlockedIncrement(&m_count);
-		}
-	}
-	
-	WRegistry(WRegistry&& nRegistery)
-		: m_count(0)
-	{
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			delete m_registered[i];
-		}
-		for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
-		{
+	long GetCount(void) const;
 
-			this->Register(*nRegistery.m_registered[i]);
-			_InterlockedIncrement(&m_count);
-		}
-	}
-	
-	~WRegistry()
-	{
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			delete m_registered[i];
-		}
-	}
-	
-	long GetCount(void) const
-	{
-		return m_count;
-	}
-
-	WRegistry* operator()(WEntity* sender, WEventArgs* args)
-	{
-		Annex current;
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			if (m_registered[i] != nullptr)
-			{
-				current = *m_registered[i];
-				current(sender, args);
-			}
-		}
-		return this;
-	}
-
-	WRegistry* Run(WEntity* sender, WEventArgs* args)
-	{
-		Annex current;
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			if (m_registered[i] != nullptr)
-			{
-				current = *m_registered[i];
-				current(sender, args);
-			}
-		}
-		return this;
-	}
-
-	WRegistry* Register(Annex rhs)
-	{
-		m_registered.push_back(new Annex(rhs));
-		_InterlockedIncrement(&m_count);
-		return this;
-	}
-
-	WRegistry* UnRegister(Annex* rhs)
-	{
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			if (m_registered[i] == rhs)
-			{
-				m_registered[i] = nullptr;
-				_InterlockedDecrement(&m_count);
-			}
-		}
-		return this;
-	}
-
-	WRegistry* operator+=(Annex rhs)
-	{
-		m_registered.push_back(new Annex(rhs));
-		_InterlockedIncrement(&m_count);
-		return this;
-	}
-
-	WRegistry* operator-=(Annex rhs)
-	{
-		for (size_t i = 0; i < m_registered.size(); i++)
-		{
-			if (m_registered[i] == &rhs)
-			{
-				m_registered[i] = nullptr;
-				_InterlockedDecrement(&m_count);
-			}
-		}
-		return this;
-	}
+	WRegistry* operator()(WEntity* sender, WEventArgs* args);
+	WRegistry* Run(WEntity* sender, WEventArgs* args);
+	WRegistry* Register(Annex rhs);
+	WRegistry* UnRegister(Annex* rhs);
+	WRegistry* operator+=(Annex rhs);
+	WRegistry* operator-=(Annex rhs);
 
 private:
 	volatile long m_count;
