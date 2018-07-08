e552a60114fc171caeffe6413245ecf2c419fa7e
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:31:56 +0430

    Change: Seperated the definition from declaration of WContainer

---
+++ src/Warp-Framework/WContainer.cpp
@@ -0,0 +1,211 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WContainer.h"
+
+const HRESULT WContainer::hResult(HRESULT intake)
+{
+	WContainer::HR = intake; return WContainer::HR;
+}
+
+const HRESULT WContainer::hResult(void)
+{
+	return WContainer::HR;
+}
+
+const HWND WContainer::Handle(HWND intake)
+{
+	WContainer::hWnd = intake;
+	return WContainer::hWnd;
+}
+
+const HWND WContainer::Handle(void)
+{
+	return WContainer::hWnd;
+}
+
+const UINT WContainer::Message(UINT intake)
+{
+	WContainer::msg = intake; 
+	return WContainer::msg;
+}
+
+const UINT WContainer::Message(void)
+{
+	return WContainer::msg;
+}
+
+const WPARAM WContainer::WParam(WPARAM intake)
+{
+	WContainer::wParam = intake;
+	return WContainer::wParam;
+}
+
+const WPARAM WContainer::WParam(void)
+{
+	return WContainer::wParam;
+}
+
+const LPARAM WContainer::LParam(LPARAM intake)
+{
+	WContainer::lParam = intake; 
+	return WContainer::lParam;
+}
+
+const LPARAM WContainer::LParam(void)
+{
+	return WContainer::lParam;
+}
+
+const WEntry WContainer::Framework(WEntry intake)
+{
+	WContainer::WFramework = intake; 
+	return WContainer::WFramework;
+}
+
+const WEntry WContainer::Framework(void)
+{
+	return WContainer::WFramework;
+}
+
+const DELTATIME WContainer::DeltaSeconds(DELTATIME intake)
+{
+	WContainer::DeltaTime = intake;
+	return WContainer::DeltaTime;
+}
+
+const DELTATIME WContainer::DeltaSeconds(void)
+{
+	return WContainer::DeltaTime;
+}
+
+const W_INT WContainer::BackA(W_INT intake)
+{
+	WContainer::BGA = intake;
+	return WContainer::BGA;
+}
+
+const W_INT WContainer::BackA(void)
+{
+	return WContainer::BGA;
+}
+
+const W_INT WContainer::BackR(W_INT intake)
+{
+	WContainer::BGR = intake;
+	return WContainer::BGR;
+}
+
+const W_INT WContainer::BackR(void)
+{
+	return WContainer::BGR;
+}
+
+const W_INT WContainer::BackG(W_INT intake)
+{
+	WContainer::BGG = intake; 
+	return WContainer::BGG;
+}
+
+const W_INT WContainer::BackG(void)
+{
+	return WContainer::BGG;
+}
+
+const W_INT WContainer::BackB(W_INT intake)
+{
+	WContainer::BGB = intake; 
+	return WContainer::BGB;
+}
+
+const W_INT WContainer::BackB(void)
+{
+	return WContainer::BGB;
+}
+
+const W_INT WContainer::Width(W_INT intake)
+{
+	WContainer::W_WIDTH = intake;
+	return WContainer::W_WIDTH;
+}
+
+const W_INT WContainer::Width(void)
+{
+	return WContainer::W_WIDTH;
+}
+
+const W_INT WContainer::Height(W_INT intake)
+{
+	WContainer::W_HEIGHT = intake;
+	return WContainer::W_HEIGHT;
+}
+
+const W_INT WContainer::Height(void)
+{
+	return WContainer::W_HEIGHT;
+}
+
+WMouse* WContainer::Mouse(WMouse* intake)
+{
+	WContainer::mouse = intake; 
+	return WContainer::mouse;
+}
+
+WMouse* WContainer::Mouse(void)
+{
+	return WContainer::mouse;
+}
+
+WKeyboard* WContainer::Keyboard(WKeyboard* intake)
+{
+	WContainer::keyboard = intake;
+	return WContainer::keyboard;
+}
+
+WKeyboard* WContainer::Keyboard(void)
+{
+	return WContainer::keyboard;
+}
+
+INT WContainer::HCX(INT intake)
+{
+	WContainer::helperCoordX = intake; 
+	return WContainer::helperCoordX;
+}
+
+INT WContainer::HCX(void)
+{
+	return WContainer::helperCoordX;
+}
+
+INT WContainer::HCY(INT intake)
+{
+	WContainer::helperCoordY = intake; 
+	return WContainer::helperCoordY;
+}
+
+INT WContainer::HCY(void)
+{
+	return WContainer::helperCoordY;
+}
+
+BOOL WContainer::DragMove(BOOL intake)
+{
+	WContainer::bDragMove = intake; 
+	return WContainer::bDragMove;
+}
+
+BOOL WContainer::DragMove(void)
+{
+	return WContainer::bDragMove;
+}
+
+WTheme WContainer::Theme(WTheme intake)
+{
+	wTheme = intake; 
+	return wTheme;
+}
+
+WTheme WContainer::Theme(void)
+{
+	return wTheme;
+}
--- src/Warp-Framework/WContainer.h
+++ src/Warp-Framework/WContainer.h
@@ -13,62 +13,62 @@
 class WContainer
 {
 public:
-	static const HRESULT hResult(HRESULT intake) { WContainer::HR = intake; return WContainer::HR; }
-	static const HRESULT hResult(void) { return WContainer::HR; }
+	static const HRESULT hResult(HRESULT intake);
+	static const HRESULT hResult(void);
 
-	static const HWND Handle(HWND intake) { WContainer::hWnd = intake; return WContainer::hWnd; }
-	static const HWND Handle(void) { return WContainer::hWnd; }
+	static const HWND Handle(HWND intake);
+	static const HWND Handle(void);
 
-	static const UINT Message(UINT intake) { WContainer::msg = intake; return WContainer::msg; }
-	static const UINT Message(void) { return WContainer::msg; }
+	static const UINT Message(UINT intake);
+	static const UINT Message(void);
 
-	static const WPARAM WParam(WPARAM intake) { WContainer::wParam = intake; return WContainer::wParam; }
-	static const WPARAM WParam(void) { return WContainer::wParam; }
+	static const WPARAM WParam(WPARAM intake);
+	static const WPARAM WParam(void);
 
-	static const LPARAM LParam(LPARAM intake) { WContainer::lParam = intake; return WContainer::lParam; }
-	static const LPARAM LParam(void) { return WContainer::lParam; }
+	static const LPARAM LParam(LPARAM intake);
+	static const LPARAM LParam(void);
 
-	static const WEntry Framework(WEntry intake) { WContainer::WFramework = intake; return WContainer::WFramework; }
-	static const WEntry Framework(void) { return WContainer::WFramework; }
+	static const WEntry Framework(WEntry intake);
+	static const WEntry Framework(void);
 
-	static const DELTATIME DeltaSeconds(DELTATIME intake) { WContainer::DeltaTime = intake; return WContainer::DeltaTime; }
-	static const DELTATIME DeltaSeconds(void) { return WContainer::DeltaTime; }
+	static const DELTATIME DeltaSeconds(DELTATIME intake);
+	static const DELTATIME DeltaSeconds(void);
 
-	static const W_INT BackA(W_INT intake) { WContainer::BGA = intake; return WContainer::BGA; }
-	static const W_INT BackA(void) { return WContainer::BGA; }
+	static const W_INT BackA(W_INT intake);
+	static const W_INT BackA(void);
 
-	static const W_INT BackR(W_INT intake) { WContainer::BGR = intake; return WContainer::BGR; }
-	static const W_INT BackR(void) { return WContainer::BGR; }
+	static const W_INT BackR(W_INT intake);
+	static const W_INT BackR(void);
 
-	static const W_INT BackG(W_INT intake) { WContainer::BGG = intake; return WContainer::BGG; }
-	static const W_INT BackG(void) { return WContainer::BGG; }
+	static const W_INT BackG(W_INT intake);
+	static const W_INT BackG(void);
 
-	static const W_INT BackB(W_INT intake) { WContainer::BGB = intake; return WContainer::BGB; }
-	static const W_INT BackB(void) { return WContainer::BGB; }
+	static const W_INT BackB(W_INT intake);
+	static const W_INT BackB(void);
 
-	static const W_INT Width(W_INT intake) { WContainer::W_WIDTH = intake; return WContainer::W_WIDTH; }
-	static const W_INT Width(void) { return WContainer::W_WIDTH; }
+	static const W_INT Width(W_INT intake);
+	static const W_INT Width(void);
 
-	static const W_INT Height(W_INT intake) { WContainer::W_HEIGHT = intake; return WContainer::W_HEIGHT; }
-	static const W_INT Height(void) { return WContainer::W_HEIGHT; }
+	static const W_INT Height(W_INT intake);
+	static const W_INT Height(void);
 
-	static WMouse* Mouse(WMouse* intake) { WContainer::mouse = intake; return WContainer::mouse; }
-	static WMouse* Mouse(void) { return WContainer::mouse; }
+	static WMouse* Mouse(WMouse* intake);
+	static WMouse* Mouse(void);
 
-	static WKeyboard* Keyboard(WKeyboard* intake) { WContainer::keyboard = intake; return WContainer::keyboard; }
-	static WKeyboard* Keyboard(void) { return WContainer::keyboard; }
+	static WKeyboard* Keyboard(WKeyboard* intake);
+	static WKeyboard* Keyboard(void);
 
-	static INT HCX(INT intake) { WContainer::helperCoordX = intake; return WContainer::helperCoordX; }
-	static INT HCX(void) { return WContainer::helperCoordX; }
+	static INT HCX(INT intake);
+	static INT HCX(void);
 		   
-	static INT HCY(INT intake) { WContainer::helperCoordY = intake; return WContainer::helperCoordY; }
-	static INT HCY(void) { return WContainer::helperCoordY; }
+	static INT HCY(INT intake);
+	static INT HCY(void);
 
-	static BOOL DragMove(BOOL intake) { WContainer::bDragMove = intake; return WContainer::bDragMove; }
-	static BOOL DragMove(void) { return WContainer::bDragMove; }
+	static BOOL DragMove(BOOL intake);
+	static BOOL DragMove(void);
 
-	static WTheme Theme(WTheme intake) { wTheme = intake; return wTheme; }
-	static WTheme Theme(void) { return wTheme; }
+	static WTheme Theme(WTheme intake);
+	static WTheme Theme(void);
 
 private:
 	static HRESULT WContainer::HR;
