69dffb225c29e42bb4d7070a567cdda1167f2c79
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:32:52 +0430

    Change: Seperated the definition from declaration of WKeyboard

---
+++ src/Warp-Framework/WKeyboard.cpp
@@ -0,0 +1,118 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WKeyboard.h"
+
+WKeyboard::WKeyboard()
+	: m_bAutorepeatEnabled(false)
+{
+	m_KeyDownReg = new WUniqueRegister();
+	m_KeyUpReg = new WUniqueRegister();
+	m_OnCharReg = new WUniqueRegister();
+	SetGRegisters();
+}
+
+WKeyboard::WKeyboard(const WKeyboard& newKeyboard)
+	: m_KeyDownReg(newKeyboard.m_KeyDownReg)
+	, m_KeyUpReg(newKeyboard.m_KeyUpReg)
+	, m_OnCharReg(newKeyboard.m_OnCharReg)
+	, m_bAutorepeatEnabled(newKeyboard.m_bAutorepeatEnabled)
+{
+	SetGRegisters();
+}
+
+WKeyboard::~WKeyboard()
+{
+	delete m_KeyDownReg;
+	delete m_KeyUpReg;
+	delete m_OnCharReg;
+}
+
+WKeyboard* WKeyboard::RunKeyDown(void)
+{
+	WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
+	this->m_KeyDownReg->Run(this, ARGS);
+	return this;
+}
+
+WKeyboard* WKeyboard::RunKeyUp(void)
+{
+	WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
+	this->m_KeyUpReg->Run(this, ARGS);
+	return this;
+}
+
+WKeyboard* WKeyboard::RunOnChar(void)
+{
+	WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
+	this->m_OnCharReg->Run(this, ARGS);
+	return this;
+}
+
+WKey WKeyboard::LastKey(void) const
+{
+	return this->m_lastKey;
+}
+
+WKeyboard* WKeyboard::LastKey(WKey intake)
+{
+	this->m_lastKey = intake;
+	return this;
+}
+
+WUniqueRegister* WKeyboard::KeyDownRegistry(void)
+{
+	return this->m_KeyDownReg;
+}
+
+WKeyboard* WKeyboard::KeyDownRegistry(WUniqueRegister* const intake)
+{
+	this->m_KeyDownReg = intake;
+	return this;
+}
+
+WUniqueRegister* WKeyboard::KeyUpRegistry(void)
+{
+	return this->m_KeyUpReg;
+}
+
+WKeyboard* WKeyboard::KeyUpRegistry(WUniqueRegister* const intake)
+{
+	this->m_KeyUpReg = intake;
+	return this;
+}
+
+WUniqueRegister* WKeyboard::OnCharRegistry(void)
+{
+	return this->m_OnCharReg;
+}
+
+WKeyboard* WKeyboard::OnCharRegistry(WUniqueRegister* const intake)
+{
+	this->m_OnCharReg = intake;
+	return this;
+}
+
+WKeyboard* WKeyboard::EnableAutorepeat(void)
+{
+	this->m_bAutorepeatEnabled = true;
+	return this;
+}
+
+WKeyboard* WKeyboard::DisableAutorepeat(void)
+{
+	this->m_bAutorepeatEnabled = true;
+	return this;
+}
+
+bool WKeyboard::AutorepeatIsEnabled(void) const
+{
+
+	return this->m_bAutorepeatEnabled;
+}
+
+void WKeyboard::SetGRegisters(void)
+{
+	WRegContainer::KeyboardKeyDown(m_KeyDownReg);
+	WRegContainer::KeyboardKeyUp(m_KeyUpReg);
+	WRegContainer::KeyboardOnChar(m_OnCharReg);
+}
--- src/Warp-Framework/WKeyboard.h
+++ src/Warp-Framework/WKeyboard.h
@@ -7,128 +7,40 @@
 #include "WUniqueRegister.h"
 #include "WKeyboardArgs.h"
 #include "WRegContainer.h"
+#include "WDefines.h"
 
 typedef W_BYTE WKey;
 
 class WKeyboard final : public WEntity
 {
 public:
-	inline WKeyboard()
-		: m_bAutorepeatEnabled(false)
-	{
-		m_KeyDownReg = new WUniqueRegister();
-		m_KeyUpReg = new WUniqueRegister();
-		m_OnCharReg = new WUniqueRegister();
-		SetGRegisters();
-	}
+	WKeyboard();
+	WKeyboard(const WKeyboard& newKeyboard);
+	~WKeyboard();
 
-	inline WKeyboard(const WKeyboard& newKeyboard)
-		: m_KeyDownReg(newKeyboard.m_KeyDownReg)
-		, m_KeyUpReg(newKeyboard.m_KeyUpReg)
-		, m_OnCharReg(newKeyboard.m_OnCharReg)
-		, m_bAutorepeatEnabled(newKeyboard.m_bAutorepeatEnabled)
-	{
-		SetGRegisters();
-	}
+	WKeyboard* RunKeyDown(void);
+	WKeyboard* RunKeyUp(void);
+	WKeyboard* RunOnChar(void);
 
-	inline ~WKeyboard()
-	{
-		delete m_KeyDownReg;
-		delete m_KeyUpReg;
-		delete m_OnCharReg;
-	}
+	WKey LastKey(void) const;
+	WKeyboard* LastKey(WKey intake);
 
-	inline WKeyboard* RunKeyDown(void)
-	{
-		WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
-		this->m_KeyDownReg->Run(this, ARGS);
-		return this;
-	}
+	WUniqueRegister* KeyDownRegistry(void);
+	WKeyboard* KeyDownRegistry(WUniqueRegister* const intake);
 
-	inline WKeyboard* RunKeyUp(void)
-	{
-		WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
-		this->m_KeyUpReg->Run(this, ARGS);
-		return this;
-	}
+	WUniqueRegister* KeyUpRegistry(void);
+	WKeyboard* KeyUpRegistry(WUniqueRegister* const intake);
 
-	inline WKeyboard* RunOnChar(void)
-	{
-		WKeyboardArgs* ARGS = new WKeyboardArgs(this->m_lastKey);
-		this->m_OnCharReg->Run(this, ARGS);
-		return this;
-	}
+	WUniqueRegister* OnCharRegistry(void);
+	WKeyboard* OnCharRegistry(WUniqueRegister* const intake);
 
+	WKeyboard* EnableAutorepeat(void);
+	WKeyboard* DisableAutorepeat(void);
 
-	inline WKey LastKey(void) const
-	{
-		return this->m_lastKey;
-	}
-
-	inline WKeyboard* LastKey(WKey intake)
-	{
-		this->m_lastKey = intake;
-		return this;
-	}
-
-	inline WUniqueRegister* KeyDownRegistry(void)
-	{
-		return this->m_KeyDownReg;
-	}
-
-	inline WKeyboard* KeyDownRegistry(WUniqueRegister* const intake)
-	{
-		this->m_KeyDownReg = intake;
-		return this;
-	}
-
-	inline WUniqueRegister* KeyUpRegistry(void)
-	{
-		return this->m_KeyUpReg;
-	}
-
-	inline WKeyboard* KeyUpRegistry(WUniqueRegister* const intake)
-	{
-		this->m_KeyUpReg = intake;
-		return this;
-	}
-
-	inline WUniqueRegister* OnCharRegistry(void)
-	{
-		return this->m_OnCharReg;
-	}
-
-	inline WKeyboard* OnCharRegistry(WUniqueRegister* const intake)
-	{
-		this->m_OnCharReg = intake;
-		return this;
-	}
-
-	inline WKeyboard* EnableAutorepeat(void)
-	{
-		this->m_bAutorepeatEnabled = true;
-		return this;
-	}
-
-	inline WKeyboard* DisableAutorepeat(void)
-	{
-		this->m_bAutorepeatEnabled = true;
-		return this;
-	}
-
-	inline bool AutorepeatIsEnabled(void) const
-	{
-
-		return this->m_bAutorepeatEnabled;
-	}
+	bool AutorepeatIsEnabled(void) const;
 
 private:
-	void SetGRegisters(void)
-	{
-		WRegContainer::KeyboardKeyDown(m_KeyDownReg);
-		WRegContainer::KeyboardKeyUp(m_KeyUpReg);
-		WRegContainer::KeyboardOnChar(m_OnCharReg);
-	}
+	void SetGRegisters(void);
 
 	bool m_bAutorepeatEnabled;
 
