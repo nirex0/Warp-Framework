95c660db0710906ade12efc2b32bec6c591ea47a
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:31:43 +0430

    Change: Seperated the definition from declaration of WColorTransform

---
+++ src/Warp-Framework/WColorTransform.cpp
@@ -0,0 +1,188 @@
+#include "WColorTransform.h"
+
+WColorTransform::WColorTransform(W_COLOR From, W_COLOR To, W_FLOAT alpha, W_LONG Delay)
+	: m_from(From)
+	, m_to(To)
+	, m_alpha(alpha)
+	, m_delay(Delay)
+	, m_value(From)
+{
+	thr = new std::thread();
+
+	m_CTTickRegistry = new WRegistry();
+	m_CTDoneRegistry = new WRegistry();
+
+	m_isLocked = false;
+}
+
+WColorTransform::~WColorTransform()
+{
+	delete thr;
+	delete m_CTTickRegistry;
+	delete m_CTDoneRegistry;
+}
+
+WRegistry* WColorTransform::TickRegistry(void) const
+{
+	return m_CTTickRegistry;
+}
+
+WRegistry* WColorTransform::DoneRegistry(void) const
+{
+	return m_CTDoneRegistry;
+}
+
+W_LONG WColorTransform::Delay(void) const
+{
+	return m_delay;
+}
+
+W_FLOAT WColorTransform::Alpha(void) const
+{
+	return m_alpha;
+}
+
+W_COLOR WColorTransform::Value(void) const
+{
+	return m_value;
+}
+
+W_COLOR WColorTransform::From(void) const
+{
+	return m_from;
+}
+
+W_COLOR WColorTransform::To(void) const
+{
+	return m_to;
+}
+
+bool WColorTransform::IsRunning(void)
+{
+	return m_isRunning;
+}
+
+WRegistry* WColorTransform::TickRegistry(WRegistry* intake)
+{
+	m_CTTickRegistry = intake; return m_CTTickRegistry;
+}
+
+WRegistry* WColorTransform::DoneRegistry(WRegistry* intake)
+{
+	m_CTDoneRegistry = intake; return m_CTDoneRegistry;
+}
+
+W_LONG WColorTransform::Delay(W_LONG intake)
+{
+	m_delay = intake;
+	return m_delay;
+}
+
+W_FLOAT WColorTransform::Alpha(W_FLOAT intake)
+{
+	m_alpha = intake; 
+	return m_alpha;
+}
+
+W_COLOR WColorTransform::Value(W_COLOR intake)
+{
+	m_value = intake; 
+	return m_value;
+}
+
+W_COLOR WColorTransform::From(W_COLOR intake)
+{
+	m_from = intake; 
+	return m_from;
+}
+
+W_COLOR WColorTransform::To(W_COLOR intake)
+{
+	m_to = intake; 
+	return m_to;
+}
+
+void WColorTransform::Perform(void)
+{
+	m_value = m_from;
+	if (WorkThread(*thr))
+	{
+		thr->detach();
+	}
+}
+
+void WColorTransform::PerformSafe(void)
+{
+	if (!m_isRunning)
+	{
+		m_value = m_from;
+		if (WorkThread(*thr))
+		{
+			thr->detach();
+		}
+	}
+}
+
+bool WColorTransform::IsLocked(void) const
+{
+	return m_isLocked;
+}
+
+void WColorTransform::Lock(void)
+{
+	m_isLocked = true;
+}
+
+void WColorTransform::Unlock(void)
+{
+	m_isLocked = false;
+}
+
+void WColorTransform::Stop(void)
+{
+	m_stop = true;
+}
+
+void WColorTransform::WorkerWork(void)
+{
+	m_isRunning = true;
+	while (
+		!isNear(m_value.r, m_to.r, 0.01F) ||
+		!isNear(m_value.g, m_to.g, 0.01F) ||
+		!isNear(m_value.b, m_to.b, 0.01F) ||
+		!isNear(m_value.a, m_to.a, 0.01F))
+	{
+		if (m_stop)
+		{
+			m_stop = false;
+			m_isRunning = false;
+			WColorTransformArgs* CTArgsDone = new WColorTransformArgs(m_to);
+			m_CTDoneRegistry->Run(this, CTArgsDone);
+			return;
+		}
+		if (m_isLocked)
+		{
+			break;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
+		std::lock_guard<std::mutex> lock(m_MutexLock);
+
+		m_value.r = m_value.r + (m_to.r - m_value.r)* m_alpha;
+		m_value.g = m_value.g + (m_to.g - m_value.g)* m_alpha;
+		m_value.b = m_value.b + (m_to.b - m_value.b)* m_alpha;
+		m_value.a = m_value.a + (m_to.a - m_value.a)* m_alpha;
+
+		WColorTransformArgs* CTArgsTick = new WColorTransformArgs(m_value);
+		m_CTTickRegistry->Run(this, CTArgsTick);
+	}
+	m_isRunning = false;
+	WColorTransformArgs* CTArgsDone = new WColorTransformArgs(m_to);
+	m_CTDoneRegistry->Run(this, CTArgsDone);
+}
+
+bool WColorTransform::WorkThread(std::thread & out)
+{
+	out = std::thread([=] { WorkerWork(); });
+	return true;
+}
--- src/Warp-Framework/WColorTransform.h
+++ src/Warp-Framework/WColorTransform.h
@@ -1,7 +1,7 @@
 // © 2018 NIREX ALL RIGHTS RESERVED
 
-#ifndef _W_COLOR_TRANSFORM_H__H_
-#define _W_COLOR_TRANSFORM_H__H_
+#ifndef _W_COLOR_TRANSFORM_H_
+#define _W_COLOR_TRANSFORM_H_
 
 #include "WEntity.h"
 #include "WColorTransformArgs.h"
@@ -18,120 +18,43 @@
 class WColorTransform : public WEntity
 {
 public:
-	WColorTransform(W_COLOR From, W_COLOR To, W_FLOAT alpha, W_LONG Delay)
-		: m_from(From)
-		, m_to(To)
-		, m_alpha(alpha)
-		, m_delay(Delay)
-		, m_value(From)
-	{
-		thr = new std::thread();
+	WColorTransform(W_COLOR From, W_COLOR To, W_FLOAT alpha, W_LONG Delay);
+	~WColorTransform();
 
-		m_CTTickRegistry = new WRegistry();
-		m_CTDoneRegistry = new WRegistry();
+	// Getters
+	WRegistry* TickRegistry(void) const;
+	WRegistry* DoneRegistry(void) const;
 
-		m_isLocked = false;
-	}
+	W_LONG Delay(void) const;
+	W_FLOAT Alpha(void) const;
+	W_COLOR Value(void) const;
+	W_COLOR From(void) const;
+	W_COLOR To(void) const;
+	bool IsRunning(void);
 
-	~WColorTransform()
-	{
-		delete thr;
-		delete m_CTTickRegistry;
-		delete m_CTDoneRegistry;
-	}
+	// Setters
+	WRegistry* TickRegistry(WRegistry* intake);
+	WRegistry* DoneRegistry(WRegistry* intake);
 
-// Getters
-	WRegistry* TickRegistry(void) const { return m_CTTickRegistry; }
-	WRegistry* DoneRegistry(void) const { return m_CTDoneRegistry; }
-
-	W_LONG Delay(void) const { return m_delay; }
-	W_FLOAT Alpha(void) const { return m_alpha; }
-	W_COLOR Value(void) const { return m_value; }
-	W_COLOR From(void) const { return m_from; }
-	W_COLOR To(void) const { return m_to; }
-	bool IsRunning(void) { return m_isRunning; }
-
-// Setters
-	WRegistry* TickRegistry(WRegistry* intake) { m_CTTickRegistry = intake; return m_CTTickRegistry; }
-	WRegistry* DoneRegistry(WRegistry* intake) { m_CTDoneRegistry = intake; return m_CTDoneRegistry; }
-
-	W_LONG Delay(W_LONG intake) { m_delay = intake; return m_delay; }
-	W_FLOAT Alpha(W_FLOAT intake) { m_alpha = intake; return m_alpha; }
-	W_COLOR Value(W_COLOR intake) { m_value = intake; return m_value; }
-	W_COLOR From(W_COLOR intake) { m_from = intake; return m_from; }
-	W_COLOR To(W_COLOR intake) { m_to = intake; return m_to; }
+	W_LONG Delay(W_LONG intake);
+	W_FLOAT Alpha(W_FLOAT intake);
+	W_COLOR Value(W_COLOR intake);
+	W_COLOR From(W_COLOR intake);
+	W_COLOR To(W_COLOR intake);
 	
 // Functions
-	void Perform(void)
-	{
-		m_value = m_from;
-		if (WorkThread(*thr))
-		{
-			thr->detach();
-		}
-	}
+	void Perform(void);
+	void PerformSafe(void);
 
-	void PerformSafe(void)
-	{
-		if (!m_isRunning)
-		{
-			m_value = m_from;
-			if (WorkThread(*thr))
-			{
-				thr->detach();
-			}
-		}
-	}
-
-	bool IsLocked(void) const { return m_isLocked; }
-	void Lock(void) { m_isLocked = true; }
-	void Unlock(void) { m_isLocked = false; }
-	void Stop(void) { m_stop = true; }
+	bool IsLocked(void) const;
+	void Lock(void);
+	void Unlock(void);
+	void Stop(void);
 
 private:
-	void WorkerWork(void)
-	{
-		m_isRunning = true;
-		while (
-			!isNear(m_value.r, m_to.r, 0.01F) ||
-			!isNear(m_value.g, m_to.g, 0.01F) ||
-			!isNear(m_value.b, m_to.b, 0.01F) ||
-			!isNear(m_value.a, m_to.a, 0.01F))
-		{
-			if (m_stop)
-			{
-				m_stop = false;
-				m_isRunning = false;
-				WColorTransformArgs* CTArgsDone = new WColorTransformArgs(m_to);
-				m_CTDoneRegistry->Run(this, CTArgsDone);
-				return;
-			}
-			if (m_isLocked)
-			{
-				break;
-			}
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
-			std::lock_guard<std::mutex> lock(m_MutexLock);
-
-			m_value.r = m_value.r + (m_to.r - m_value.r) * m_alpha;
-			m_value.g = m_value.g + (m_to.g - m_value.g) * m_alpha;
-			m_value.b = m_value.b + (m_to.b - m_value.b) * m_alpha;
-			m_value.a = m_value.a + (m_to.a - m_value.a) * m_alpha;
-
-			WColorTransformArgs* CTArgsTick = new WColorTransformArgs(m_value);
-			m_CTTickRegistry->Run(this, CTArgsTick);
-		}
-		m_isRunning = false;
-		WColorTransformArgs* CTArgsDone = new WColorTransformArgs(m_to);
-		m_CTDoneRegistry->Run(this, CTArgsDone);
-	}
-
-	bool WorkThread(std::thread& out)
-	{
-		out = std::thread([=] { WorkerWork(); });
-		return true;
-	}
+	void WorkerWork(void);
+	bool WorkThread(std::thread& out);
+	
 
 private:
 	W_LONG m_delay;
