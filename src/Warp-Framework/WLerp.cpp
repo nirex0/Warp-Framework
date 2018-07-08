7cba4a9107b3f560a0ed8f5327c3cbb1a9bdd3d6
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:33:03 +0430

    Change: Seperated the definition from declaration of WLerp

---
+++ src/Warp-Framework/WLerp.cpp
@@ -0,0 +1,181 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WLerp.h"
+
+WLerp::WLerp(W_FLOAT from, W_FLOAT to, W_FLOAT alpha, W_LONG delay)
+	: m_from(from)
+	, m_to(to)
+	, m_alpha(alpha)
+	, m_value(from)
+	, m_delay(delay)
+{
+	thr = new std::thread();
+
+	m_LerpTickRegistry = new WRegistry();
+	m_LerpDoneRegistry = new WRegistry();
+
+	m_isLocked = false;
+}
+
+WLerp::~WLerp()
+{
+	delete thr;
+	delete m_LerpTickRegistry;
+	delete m_LerpDoneRegistry;
+}
+
+WRegistry* WLerp::TickRegistry(void) const
+{
+	return m_LerpTickRegistry;
+}
+
+WRegistry* WLerp::DoneRegistry(void) const
+{
+	return m_LerpDoneRegistry;
+}
+
+
+
+W_LONG WLerp::Delay(void) const
+{
+	return m_delay;
+}
+
+W_FLOAT WLerp::Value(void) const
+{
+	return m_value;
+}
+
+W_FLOAT WLerp::From(void) const
+{
+	return m_from;
+}
+
+W_FLOAT WLerp::To(void) const
+{
+	return m_to;
+}
+
+W_FLOAT WLerp::Alpha(void) const
+{
+	return m_alpha;
+}
+
+bool WLerp::IsRunning(void)
+{
+	return m_isRunning;
+}
+
+WRegistry* WLerp::TickRegistry(WRegistry* intake)
+{
+	m_LerpTickRegistry = intake; 
+	return m_LerpTickRegistry;
+}
+
+WRegistry* WLerp::DoneRegistry(WRegistry* intake)
+{
+	m_LerpDoneRegistry = intake; 
+	return m_LerpDoneRegistry;
+}
+
+W_LONG WLerp::Delay(W_LONG intake)
+{
+	m_delay = intake;
+	return m_delay;
+}
+
+W_FLOAT WLerp::Value(W_FLOAT intake)
+{
+	m_value = intake; 
+	return m_value;
+}
+
+W_FLOAT WLerp::From(W_FLOAT intake)
+{
+	m_from = intake; 
+	return m_from;
+}
+
+W_FLOAT WLerp::To(W_FLOAT intake)
+{
+	m_to = intake; 
+	return m_to;
+}
+
+W_FLOAT WLerp::Alpha(W_FLOAT intake)
+{
+	m_alpha = intake; 
+	return m_alpha;
+}
+
+void WLerp::Perform(void)
+{
+	m_value = m_from;
+	if (WorkThread(*thr))
+	{
+		thr->detach();
+	}
+}
+
+void WLerp::PerformSafe(void)
+{
+	if (!m_isRunning)
+	{
+		m_value = m_from;
+		if (WorkThread(*thr))
+		{
+			thr->detach();
+		}
+	}
+}
+
+bool WLerp::IsLocked(void) const
+{
+	return m_isLocked;
+}
+
+void WLerp::Lock(void)
+{
+	m_isLocked = true;
+}
+
+void WLerp::Unlock(void)
+{
+	m_isLocked = false;
+}
+
+void WLerp::Stop(void)
+{
+	m_stop = true;
+}
+
+void WLerp::WorkerWork(void)
+{
+	m_isRunning = true;
+	while (!isNear(m_value, m_to, 1) && !m_isLocked)
+	{
+		if (m_stop)
+		{
+			m_stop = false;
+			m_isRunning = false;
+			WLerpArgs* lerpArgsDone = new WLerpArgs(m_to);
+			m_LerpDoneRegistry->Run(this, lerpArgsDone);
+			return;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
+		m_value = m_value + (m_to - m_value)* m_alpha;
+
+		WLerpArgs* lerpArgsTick = new WLerpArgs(m_value);
+		m_LerpTickRegistry->Run(this, lerpArgsTick);
+	}
+	m_isRunning = false;
+	WLerpArgs* lerpArgsDone = new WLerpArgs(m_to);
+	m_LerpDoneRegistry->Run(this, lerpArgsDone);
+}
+
+bool WLerp::WorkThread(std::thread & out)
+{
+	out = std::thread([=] { WorkerWork(); });
+	return true;
+}
--- src/Warp-Framework/WLerp.h
+++ src/Warp-Framework/WLerp.h
@@ -17,107 +17,42 @@
 class WLerp : public WEntity
 {
 public:
-	WLerp(W_FLOAT from, W_FLOAT to, W_FLOAT alpha, W_LONG delay = 1)
-		: m_from(from)
-		, m_to(to)
-		, m_alpha(alpha)
-		, m_value(from)
-		, m_delay(delay)
-	{
-		thr = new std::thread();
-
-		m_LerpTickRegistry = new WRegistry();
-		m_LerpDoneRegistry = new WRegistry();
-
-		m_isLocked = false;
-	}
-
-	~WLerp()
-	{
-		delete thr;
-		delete m_LerpTickRegistry;
-		delete m_LerpDoneRegistry;
-	}
+	WLerp(W_FLOAT from, W_FLOAT to, W_FLOAT alpha, W_LONG delay = 1);
+	~WLerp();
 
 // Getters
-	WRegistry* TickRegistry(void) const { return m_LerpTickRegistry; }
-	WRegistry* DoneRegistry(void) const { return m_LerpDoneRegistry; }
+	WRegistry* TickRegistry(void) const;
+	WRegistry* DoneRegistry(void) const;
 
-	W_LONG Delay(void) const { return m_delay; }
-	W_FLOAT Value(void) const { return m_value; }
-	W_FLOAT From(void) const { return m_from; }
-	W_FLOAT To(void) const { return m_to; }
-	W_FLOAT Alpha(void) const { return m_alpha; }
-	bool IsRunning(void) { return m_isRunning; }
+	W_LONG Delay(void) const;
+	W_FLOAT Value(void) const;
+	W_FLOAT From(void) const;
+	W_FLOAT To(void) const;
+	W_FLOAT Alpha(void) const;
+	bool IsRunning(void);
 
 // Setters
-	WRegistry* TickRegistry(WRegistry* intake) { m_LerpTickRegistry = intake; return m_LerpTickRegistry; }
-	WRegistry* DoneRegistry(WRegistry* intake) { m_LerpDoneRegistry = intake; return m_LerpDoneRegistry; }
+	WRegistry* TickRegistry(WRegistry* intake);
+	WRegistry* DoneRegistry(WRegistry* intake);
 
-	W_LONG Delay(W_LONG intake) { m_delay = intake; return m_delay; }
-	W_FLOAT Value(W_FLOAT intake) { m_value = intake; return m_value; }
-	W_FLOAT From(W_FLOAT intake) { m_from = intake; return m_from; }
-	W_FLOAT To(W_FLOAT intake) { m_to = intake; return m_to; }
-	W_FLOAT Alpha(W_FLOAT intake) { m_alpha = intake; return m_alpha; }
+	W_LONG Delay(W_LONG intake);
+	W_FLOAT Value(W_FLOAT intake);
+	W_FLOAT From(W_FLOAT intake);
+	W_FLOAT To(W_FLOAT intake);
+	W_FLOAT Alpha(W_FLOAT intake);
 
 // Functions
-	void Perform(void)
-	{
-		m_value = m_from;
-		if(WorkThread(*thr))
-		{ 
-			thr->detach();
-		}
-	}
+	void Perform(void);
+	void PerformSafe(void);
 
-	void PerformSafe(void)
-	{
-		if (!m_isRunning)
-		{
-			m_value = m_from;		
-			if (WorkThread(*thr))
-			{
-				thr->detach();
-			}
-		}
-	}
-
-	bool IsLocked(void) const { return m_isLocked; }
-	void Lock(void) { m_isLocked = true; }
-	void Unlock(void) { m_isLocked = false; }
-	void Stop(void) { m_stop = true; }
+	bool IsLocked(void) const;
+	void Lock(void);
+	void Unlock(void);
+	void Stop(void);
 
 private:
-	void WorkerWork(void)
-	{
-		m_isRunning = true;
-		while (!isNear(m_value, m_to, 1) && !m_isLocked)
-		{
-			if (m_stop)
-			{
-				m_stop = false;
-				m_isRunning = false;
-				WLerpArgs* lerpArgsDone = new WLerpArgs(m_to);
-				m_LerpDoneRegistry->Run(this, lerpArgsDone);
-				return;
-			}
-
-			std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
-			m_value = m_value + (m_to - m_value) * m_alpha;
-
-			WLerpArgs* lerpArgsTick = new WLerpArgs(m_value);
-			m_LerpTickRegistry->Run(this, lerpArgsTick);
-		}
-		m_isRunning = false;
-		WLerpArgs* lerpArgsDone = new WLerpArgs(m_to);
-		m_LerpDoneRegistry->Run(this, lerpArgsDone);
-	}
-
-	bool WorkThread(std::thread& out)
-	{
-		out = std::thread([=] { WorkerWork(); });
-		return true;
-	}
+	void WorkerWork(void);
+	bool WorkThread(std::thread& out);
 
 private:
 	W_LONG m_delay;
