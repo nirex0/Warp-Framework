380c491096d2fa0736346f44bd15423257864e86
Nirex0 <Nirex.0@gmail.com>
2018-07-08 21:33:46 +0430

    Change: Seperated the definition from declaration of WSmoothStep

---
+++ src/Warp-Framework/WSmoothStep.cpp
@@ -0,0 +1,167 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WSmoothStep.h"
+
+WSmoothStep::WSmoothStep(W_FLOAT from, W_FLOAT to, W_LONG delay)
+	: m_from(from)
+	, m_to(to)
+	, m_value(from)
+	, m_delay(delay)
+{
+	thr = new std::thread();
+
+	m_SmoothStepTickRegistry = new WRegistry();
+	m_SmoothStepDoneRegistry = new WRegistry();
+
+	m_isLocked = false;
+}
+
+WSmoothStep::~WSmoothStep()
+{
+	delete thr;
+	delete m_SmoothStepTickRegistry;
+	delete m_SmoothStepDoneRegistry;
+}
+WRegistry* WSmoothStep::TickRegistry(void) const
+{
+	return m_SmoothStepTickRegistry;
+}
+
+WRegistry* WSmoothStep::DoneRegistry(void) const
+{
+	return m_SmoothStepDoneRegistry;
+}
+
+W_LONG WSmoothStep::Delay(void) const
+{
+	return m_delay;
+}
+
+W_FLOAT WSmoothStep::Value(void) const
+{
+	return m_value;
+}
+
+W_FLOAT WSmoothStep::From(void) const
+{
+	return m_from;
+}
+
+W_FLOAT WSmoothStep::To(void) const
+{
+	return m_to;
+}
+
+bool WSmoothStep::IsRunning(void)
+{
+	return m_isRunning;
+}
+
+WRegistry* WSmoothStep::TickRegistry(WRegistry* intake)
+{
+	m_SmoothStepTickRegistry = intake;
+	return m_SmoothStepTickRegistry;
+}
+
+WRegistry* WSmoothStep::DoneRegistry(WRegistry* intake)
+{
+	m_SmoothStepDoneRegistry = intake;
+	return m_SmoothStepDoneRegistry;
+}
+
+W_LONG WSmoothStep::Delay(W_LONG intake)
+{
+	m_delay = intake;
+	return m_delay;
+}
+
+W_FLOAT WSmoothStep::Value(W_FLOAT intake)
+{
+	m_value = intake;
+	return m_value;
+}
+
+W_FLOAT WSmoothStep::From(W_FLOAT intake)
+{
+	m_from = intake;
+	return m_from;
+}
+
+W_FLOAT WSmoothStep::To(W_FLOAT intake)
+{
+	m_to = intake;
+	return m_to;
+}
+
+void WSmoothStep::Perform(void)
+{
+	m_value = m_from;
+	if (WorkThread(*thr))
+	{
+		thr->detach();
+	}
+}
+
+void WSmoothStep::PerformSafe(void)
+{
+	if (!m_isRunning)
+	{
+		m_value = m_from;
+		if (WorkThread(*thr))
+		{
+			thr->detach();
+		}
+	}
+}
+
+bool WSmoothStep::IsLocked(void) const
+{
+	return m_isLocked;
+}
+
+void WSmoothStep::Lock(void)
+{
+	m_isLocked = true;
+}
+
+void WSmoothStep::Unlock(void)
+{
+	m_isLocked = false;
+}
+
+void WSmoothStep::Stop(void)
+{
+	m_stop = true;
+}
+
+void WSmoothStep::WorkerWork(void)
+{
+	m_isRunning = true;
+	while (!isNear(m_value, m_to, 1) && !m_isLocked)
+	{
+		if (m_stop)
+		{
+			m_stop = false;
+			m_isRunning = false;
+			WSmoothStepArgs* SSArgsDone = new WSmoothStepArgs(m_to);
+			m_SmoothStepDoneRegistry->Run(this, SSArgsDone);
+			return;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
+		std::lock_guard<std::mutex> lock(m_MutexLock);
+
+		m_value = smoothstep(m_from, m_to, m_value);
+
+		WSmoothStepArgs* SSArgsTick = new WSmoothStepArgs(m_value);
+		m_SmoothStepTickRegistry->Run(this, SSArgsTick);
+	}
+	m_isRunning = false;
+	WSmoothStepArgs* SSArgsDone = new WSmoothStepArgs(m_to);
+	m_SmoothStepDoneRegistry->Run(this, SSArgsDone);
+}
+
+bool WSmoothStep::WorkThread(std::thread & out)
+{
+	out = std::thread([=] { WorkerWork(); });
+	return true;
+}
--- src/Warp-Framework/WSmoothStep.h
+++ src/Warp-Framework/WSmoothStep.h
@@ -17,105 +17,42 @@
 class WSmoothStep : public WEntity
 {
 public:
-	WSmoothStep(W_FLOAT from, W_FLOAT to, W_LONG delay = 1)
-		: m_from(from)
-		, m_to(to)
-		, m_value(from)
-		, m_delay(delay)
-	{
-		thr = new std::thread();
-
-		m_SmoothStepTickRegistry = new WRegistry();
-		m_SmoothStepDoneRegistry = new WRegistry();
-
-		m_isLocked = false;
-	}
-
-	~WSmoothStep()
-	{
-		delete thr;
-		delete m_SmoothStepTickRegistry;
-		delete m_SmoothStepDoneRegistry;
-	}
+	WSmoothStep(W_FLOAT from, W_FLOAT to, W_LONG delay = 1);
+	~WSmoothStep();
 
 // Getters
-	WRegistry* TickRegistry(void) const { return m_SmoothStepTickRegistry; }
-	WRegistry* DoneRegistry(void) const { return m_SmoothStepDoneRegistry; }
+	WRegistry* TickRegistry(void) const;
+	WRegistry* DoneRegistry(void) const;
 
-	W_LONG Delay(void) const { return m_delay; }
-	W_FLOAT Value(void) const { return m_value; }
-	W_FLOAT From(void) const { return m_from; }
-	W_FLOAT To(void) const { return m_to; }
-	bool IsRunning(void) { return m_isRunning; }
+	W_LONG Delay(void) const;
+	W_FLOAT Value(void) const;
+	W_FLOAT From(void) const;
+	W_FLOAT To(void) const;
+	bool IsRunning(void);
 
 // Setters
-	WRegistry* TickRegistry(WRegistry* intake) { m_SmoothStepTickRegistry = intake; return m_SmoothStepTickRegistry; }
-	WRegistry* DoneRegistry(WRegistry* intake) { m_SmoothStepDoneRegistry = intake; return m_SmoothStepDoneRegistry; }
-	
-	W_LONG Delay(W_LONG intake) { m_delay = intake; return m_delay; }
-	W_FLOAT Value(W_FLOAT intake) { m_value = intake; return m_value; }
-	W_FLOAT From(W_FLOAT intake) { m_from = intake; return m_from; }
-	W_FLOAT To(W_FLOAT intake) { m_to = intake; return m_to; }
+	WRegistry* TickRegistry(WRegistry* intake);
+	WRegistry* DoneRegistry(WRegistry* intake);
+
+	W_LONG Delay(W_LONG intake);
+	W_FLOAT Value(W_FLOAT intake);
+	W_FLOAT From(W_FLOAT intake);
+	W_FLOAT To(W_FLOAT intake);
 
 // Functions
-	void Perform(void)
-	{
-		m_value = m_from;
-		if(WorkThread(*thr))
-		{ 
-			thr->detach();
-		}
-	}
+	void Perform(void);
+	
+	void PerformSafe(void);
 
-	void PerformSafe(void)
-	{
-		if (!m_isRunning)
-		{
-			m_value = m_from;		
-			if (WorkThread(*thr))
-			{
-				thr->detach();
-			}
-		}
-	}
 
-	bool IsLocked(void) const { return m_isLocked; }
-	void Lock(void) { m_isLocked = true; }
-	void Unlock(void) { m_isLocked = false; }
-	void Stop(void) { m_stop = true; }
+	bool IsLocked(void) const;
+	void Lock(void);
+	void Unlock(void);
+	void Stop(void);
 
 private:
-	void WorkerWork(void)
-	{
-		m_isRunning = true;
-		while (!isNear(m_value, m_to, 1) && !m_isLocked)
-		{
-			if (m_stop)
-			{
-				m_stop = false;
-				m_isRunning = false;
-				WSmoothStepArgs* SSArgsDone = new WSmoothStepArgs(m_to);
-				m_SmoothStepDoneRegistry->Run(this, SSArgsDone);
-				return;
-			}
-			std::this_thread::sleep_for(std::chrono::milliseconds(m_delay));
-			std::lock_guard<std::mutex> lock(m_MutexLock);
-
-			m_value = smoothstep<W_FLOAT>(m_from, m_to, m_value);
-
-			WSmoothStepArgs* SSArgsTick = new WSmoothStepArgs(m_value);
-			m_SmoothStepTickRegistry->Run(this, SSArgsTick);
-		}
-		m_isRunning = false;
-		WSmoothStepArgs* SSArgsDone = new WSmoothStepArgs(m_to);
-		m_SmoothStepDoneRegistry->Run(this, SSArgsDone);
-	}
-
-	bool WorkThread(std::thread& out)
-	{
-		out = std::thread([=] { WorkerWork(); });
-		return true;
-	}
+	void WorkerWork(void);
+	bool WorkThread(std::thread& out);
 
 private:
 	W_LONG m_delay;
