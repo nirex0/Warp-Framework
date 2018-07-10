b9fa305e2984398dd8b1931d5f6d94212f5d2c58
Nirex0 <Nirex.0@gmail.com>
2018-07-11 02:05:21 +0430

    Add: WLogicalBound class

---
+++ src/Warp-Framework/WLogicalBound.cpp
@@ -0,0 +1,58 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WLogicalBound.h"
+
+WLogicalBound::WLogicalBound()
+{
+}
+
+WLogicalBound::~WLogicalBound()
+{
+}
+
+bool WLogicalBound::Binding(void) const
+{
+	return m_binding;
+}
+
+bool WLogicalBound::Binding(bool intake)
+{
+	m_binding = intake;
+	return m_binding;
+}
+
+WBinding* WLogicalBound::Function(void) const
+{
+	return m_annex;
+}
+
+WBinding* WLogicalBound::Function(WBinding intake)
+{
+	m_annex = &intake;
+	return m_annex;
+}
+
+void WLogicalBound::operator=(WBinding rhs)
+{
+	m_annex = &rhs;
+}
+
+void WLogicalBound::operator()(void)
+{
+	WBinding current;
+	if (m_annex)
+	{
+		current = *m_annex;
+		current(m_binding);
+	}
+}
+
+void WLogicalBound::Run(void)
+{
+	WBinding current;
+	if (m_annex)
+	{
+		current = *m_annex;
+		current(m_binding);
+	}
+}
---
+++ src/Warp-Framework/WLogicalBound.h
@@ -0,0 +1,37 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_LOGICAL_BOUND_H_
+#define _W_LOGICAL_BOUND_H_
+
+#include "WEntity.h"
+#include "WEventArgs.h"
+
+#include <vector>
+#include <functional>
+
+typedef std::function<void(bool&)> WBinding;
+
+class WLogicalBound : public WEntity
+{
+public:
+	WLogicalBound();
+	~WLogicalBound();
+
+	bool Binding(void) const;
+	bool Binding(bool intake);
+
+	WBinding* Function(void) const;
+	WBinding* Function(WBinding intake);
+	
+	void operator=(WBinding rhs);
+	void operator()(void);
+
+	void Run(void);
+
+private:
+	bool m_binding;
+	WBinding* m_annex;
+};
+
+
+#endif // !_W_LOGICAL_BOUND_H_
