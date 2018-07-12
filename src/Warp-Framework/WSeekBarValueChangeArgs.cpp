5c0a57c6dad3e44e00ffefa690fe477fdf37c4fa
Nirex0 <Nirex.0@gmail.com>
2018-07-12 15:50:32 +0430

    Add: ValueChange Event args for the WSeekBar Control

---
+++ src/Warp-Framework/WSeekBarValueChangeArgs.cpp
@@ -0,0 +1,13 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WSeekBarValueChangeArgs.h"
+
+WSeekBarValueChangeArgs::WSeekBarValueChangeArgs(W_FLOAT input)
+{
+	m_value = input;
+}
+
+W_FLOAT WSeekBarValueChangeArgs::Value(void) const
+{
+	return m_value;
+}
---
+++ src/Warp-Framework/WSeekBarValueChangeArgs.h
@@ -0,0 +1,21 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_SEEK_BAR_VALUECHANGE_ARGS_H_
+#define _W_SEEK_BAR_VALUECHANGE_ARGS_H_
+
+#include "WEventArgs.h"
+#include "WDefines.h"
+
+class WSeekBarValueChangeArgs : public WEventArgs
+{
+public:
+	WSeekBarValueChangeArgs(W_FLOAT input);
+	~WSeekBarValueChangeArgs() = default;
+	
+	W_FLOAT Value(void) const;
+
+private:
+	W_FLOAT m_value;
+};
+
+#endif // !_W_SEEK_BAR_VALUECHANGE_ARGS_H_
