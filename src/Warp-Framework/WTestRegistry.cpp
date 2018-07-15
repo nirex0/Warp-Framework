f92c6a5053ce091d11b3b643ac9f105090b7686d
Nirex0 <Nirex.0@gmail.com>
2018-07-15 21:41:36 +0430

    Add: WTestRegistry class to help with unit testing

---
+++ src/Warp-Framework/WTestRegistry.cpp
@@ -0,0 +1,119 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WTestRegistry.h"
+#include <cassert>
+
+WTestRegistry::WTestRegistry()
+	: m_count(0)
+{
+}
+
+WTestRegistry::WTestRegistry(const WTestRegistry& nRegistery)
+	: m_count(0)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+	for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
+	{
+		this->Register(*nRegistery.m_registered[i]);
+		_InterlockedIncrement(&m_count);
+	}
+}
+
+WTestRegistry::WTestRegistry(WTestRegistry&& nRegistery)
+	: m_count(0)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+	for (size_t i = 0; i < nRegistery.m_registered.size(); i++)
+	{
+
+		this->Register(*nRegistery.m_registered[i]);
+		_InterlockedIncrement(&m_count);
+	}
+}
+
+WTestRegistry::~WTestRegistry()
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		delete m_registered[i];
+	}
+}
+
+long WTestRegistry::GetCount(void) const
+{
+	return m_count;
+}
+
+WTestRegistry* WTestRegistry::operator()()
+{
+	WTest current;
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] != nullptr)
+		{
+			current = *m_registered[i];
+			assert(current() == true);
+		}
+	}
+	return this;
+}
+
+WTestRegistry* WTestRegistry::Test()
+{
+	WTest current;
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] != nullptr)
+		{
+			current = *m_registered[i];
+			assert(current() == true);
+		}
+	}
+	return this;
+}
+
+WTestRegistry* WTestRegistry::Register(WTest rhs)
+{
+	m_registered.push_back(new WTest(rhs));
+	_InterlockedIncrement(&m_count);
+	return this;
+}
+
+WTestRegistry* WTestRegistry::UnRegister(WTest* rhs)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] == rhs)
+		{
+			m_registered[i] = nullptr;
+			_InterlockedDecrement(&m_count);
+		}
+	}
+	return this;
+}
+
+WTestRegistry* WTestRegistry::operator+=(WTest rhs)
+{
+	m_registered.push_back(new WTest(rhs));
+	_InterlockedIncrement(&m_count);
+	return this;
+}
+
+WTestRegistry* WTestRegistry::operator-=(WTest rhs)
+{
+	for (size_t i = 0; i < m_registered.size(); i++)
+	{
+		if (m_registered[i] == &rhs)
+		{
+			m_registered[i] = nullptr;
+			_InterlockedDecrement(&m_count);
+		}
+	}
+	return this;
+}
---
+++ src/Warp-Framework/WTestRegistry.h
@@ -0,0 +1,33 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_TEST_REGISTRY_H_
+#define _W_TEST_REGISTRY_H_
+
+#include <vector>
+#include <functional>
+
+typedef std::function<bool(void)> WTest;
+
+class WTestRegistry
+{
+public:
+	WTestRegistry();
+	WTestRegistry(const WTestRegistry& nRegistery);
+	WTestRegistry(WTestRegistry&& nRegistery);
+	~WTestRegistry();
+
+	long GetCount(void) const;
+
+	WTestRegistry* operator()();
+	WTestRegistry* Test();
+	WTestRegistry* Register(WTest rhs);
+	WTestRegistry* UnRegister(WTest* rhs);
+	WTestRegistry* operator+=(WTest rhs);
+	WTestRegistry* operator-=(WTest rhs);
+
+private:
+	volatile long m_count;
+	std::vector<WTest*> m_registered;
+};
+
+#endif // !_W_TEST_REGISTRY_H_
