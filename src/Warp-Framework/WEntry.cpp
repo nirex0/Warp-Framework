2ce19e6c3a033ba1060975d222ba07e67f013926
Nirex0 <Nirex.0@gmail.com>
2018-07-18 12:35:05 +0430

    Add: RadioButtonHandler initialization in WEntry

--- src/Warp-Framework/WEntry.cpp
+++ src/Warp-Framework/WEntry.cpp
@@ -2,26 +2,25 @@
 
 #include "WContainer.h"
 #include "WControlHandler.h"
+#include "WRadioButtonHandler.h"
 
 // These needs to be initialized before everything else
 std::map<W_INT, IControl*> WControlHandler::mtcp = {};
+std::set<WRadioButton*> WRadioButtonHandler::strz = {};
 std::set<W_INT> WControlHandler::stcz = {};
 WTheme WContainer::wTheme = {};
 
 //Runs at the start of the application
 void WEntry::Start(void)
 {
-
 }
 
 //Runs every frame pre-render
 void WEntry::Update(DELTATIME deltaTime)
 {
-
 }
 
 //Runs every frame post-update
 void WEntry::Render(DELTATIME deltaTime)
 {
-
 }
