8e0244ebb30f06570fa629abbc0c49a5b4cb4c2f
Nirex0 <Nirex.0@gmail.com>
2018-07-18 13:30:47 +0430

    Major optimization: Flush is now called on WTextBox

--- src/Warp-Framework/WTextBox.cpp
+++ src/Warp-Framework/WTextBox.cpp
@@ -360,6 +360,8 @@ void WTextBox::Render(void)
 	WGraphicsContainer::Graphics()->FillRoundRectSolid(ctRec, 2, backColor);
 	WGraphicsContainer::Graphics()->WriteText(ctRec, (wchar_t*)m_Content.c_str(), (UINT32)m_Content.length(), (wchar_t*)m_family.c_str(), m_fsize, foreColor, WTA_LeftToRight, ctRec);
 
+	WGraphicsContainer::Graphics()->SafeFlush();
+
 	// End Mask Render
 	WGraphicsContainer::Graphics()->GetRenderTarget()->PopLayer();
 	SafeRelease(&maskLayer);
