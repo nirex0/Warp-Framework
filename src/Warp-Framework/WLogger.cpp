9f0d9c106660d2a8d7869036a88c92d6634c9a57
Nirex0 <Nirex.0@gmail.com>
2018-07-19 13:04:23 +0430

    Update: WLogger can now write to files

--- src/Warp-Framework/WLogger.cpp
+++ src/Warp-Framework/WLogger.cpp
@@ -1,5 +1,6 @@
 // © 2018 NIREX ALL RIGHTS RESERVED
 
+#include <fstream>
 #include "WLogger.h"
 #include "WWin.h"
 
@@ -63,4 +64,77 @@ void WLogger::Debug(std::wstring msg, int logLevel)
 	l_msg += L"\n";
 
 	OutputDebugString(l_msg.c_str());
+}
+
+void WLogger::Error(std::wstring msg, std::wstring path, int logLevel)
+{
+	if (logLevel > m_loglevel)
+		return;
+
+	std::wstring l_msg;
+	l_msg = L"(";
+	l_msg += std::to_wstring(logLevel);
+	l_msg += L") E: ";
+	l_msg += msg;
+	l_msg += L"\n";
+
+	std::ofstream file;
+	file.open(path, std::ofstream::out | std::ofstream::app);
+	file << l_msg.c_str();
+	file.close();
+}
+
+
+void WLogger::Warning(std::wstring msg, std::wstring path, int logLevel)
+{
+	if (logLevel > m_loglevel)
+		return;
+
+	std::wstring l_msg;
+	l_msg = L"(";
+	l_msg += std::to_wstring(logLevel);
+	l_msg += L") W: ";
+	l_msg += msg;
+	l_msg += L"\n";
+
+	std::ofstream file;
+	file.open(path, std::ofstream::out | std::ofstream::app);
+	file << l_msg.c_str();
+	file.close();
+}
+
+void WLogger::Information(std::wstring msg, std::wstring path, int logLevel)
+{
+	if (logLevel > m_loglevel)
+		return;
+
+	std::wstring l_msg;
+	l_msg = L"(";
+	l_msg += std::to_wstring(logLevel);
+	l_msg += L") I: ";
+	l_msg += msg;
+	l_msg += L"\n";
+
+	std::ofstream file;
+	file.open(path, std::ofstream::out | std::ofstream::app);
+	file << l_msg.c_str();
+	file.close();
+}
+
+void WLogger::Debug(std::wstring msg, std::wstring path, int logLevel)
+{
+	if (logLevel > m_loglevel)
+		return;
+
+	std::wstring l_msg;
+	l_msg = L"(";
+	l_msg += std::to_wstring(logLevel);
+	l_msg += L") D: ";
+	l_msg += msg;
+	l_msg += L"\n";
+
+	std::ofstream file;
+	file.open(path, std::ofstream::out | std::ofstream::app);
+	file << l_msg.c_str();
+	file.close();
 }
--- src/Warp-Framework/WLogger.h
+++ src/Warp-Framework/WLogger.h
@@ -13,6 +13,11 @@ public:
 	static void Information(std::wstring msg, int logLevel);
 	static void Debug(std::wstring msg, int logLevel);
 
+	static void Error(std::wstring msg, std::wstring path, int logLevel);
+	static void Warning(std::wstring msg, std::wstring path, int logLevel);
+	static void Information(std::wstring msg, std::wstring path, int logLevel);
+	static void Debug(std::wstring msg, std::wstring path, int logLevel);
+
 private:
 	static int m_loglevel;
 };
