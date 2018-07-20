02c28d31d945fc80a91f4e9ca7ceaf7c106e2e7b
Nirex0 <Nirex.0@gmail.com>
2018-07-21 01:46:32 +0430

    Add: New WGridComponent class

---
+++ src/Warp-Framework/WGridComponent.cpp
@@ -0,0 +1,204 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WGridComponent.h"
+#include "WControlHandler.h"
+#include "WSafeRelease.h"
+
+WGridComponent::WGridComponent(W_INT zIndex)
+	: WControl(zIndex)
+	, m_thickness(1.0F)
+	, m_sqSize(10.0F)
+{
+	m_color = WContainer::Theme().ColorBorder();
+}
+
+WGridComponent::WGridComponent(W_FLOAT top, W_FLOAT left, W_FLOAT bottom, W_FLOAT right, W_INT zIndex)
+	: WControl(top, left, bottom, right, zIndex)
+	, m_thickness(1.0F)
+	, m_sqSize(10.0F)
+{
+	m_color = WContainer::Theme().ColorBorder();
+}
+
+WGridComponent::WGridComponent(WPointF topleft, WPointF botright, W_INT zIndex)
+	: WControl(topleft, botright, zIndex)
+	, m_thickness(1.0F)
+	, m_sqSize(10.0F)
+{
+	m_color = WContainer::Theme().ColorBorder();
+}
+
+WGridComponent::WGridComponent(WRectF location, W_INT zIndex)
+	: WControl(location, zIndex)
+	, m_thickness(1.0F)
+	, m_sqSize(10.0F)
+{
+	m_color = WContainer::Theme().ColorBorder();
+}
+
+W_INT WGridComponent::SquareSize(W_INT f)
+{
+	if (f <= 10)
+	{
+		m_sqSize = 10;
+		return 10;
+	}
+
+	m_sqSize = f;
+	return m_sqSize;
+}
+
+W_FLOAT WGridComponent::LineThickness(W_FLOAT f)
+{
+	m_thickness = f;
+	return m_thickness;
+}
+
+W_COLOR WGridComponent::Color(W_COLOR col)
+{
+	m_color = col;
+	return m_color;
+}
+
+W_INT WGridComponent::SquareSize(void) const
+{
+	return m_sqSize;
+}
+
+W_FLOAT WGridComponent::LineThickness(void) const
+{
+	return m_thickness;
+}
+
+W_COLOR WGridComponent::Color(void) const
+{
+	return m_color;
+}
+
+void WGridComponent::Render(void)
+{
+	if (!m_isVisible)
+		return;
+
+	ID2D1Layer* maskLayer;
+	D2D_RECT_F ParentRect;
+	if (m_Parent)
+	{
+		if (!m_Parent->IsEnabled())
+			return;
+		if (!m_Parent->IsVisible())
+			return;
+
+		ParentRect.top = m_Parent->Location().Top();
+		ParentRect.left = m_Parent->Location().Left();
+		ParentRect.bottom = m_Parent->Location().Bottom();
+		ParentRect.right = m_Parent->Location().Right();
+	}
+	else
+	{
+		ParentRect.top = 0.0F;
+		ParentRect.left = 0.0F;
+		ParentRect.bottom = (W_FLOAT)INFINITE;
+		ParentRect.right = (W_FLOAT)INFINITE;
+	}
+
+	WRectF parent;
+	parent.Top(ParentRect.top);
+	parent.Left(ParentRect.left);
+	parent.Bottom(ParentRect.bottom);
+	parent.Right(ParentRect.right);
+
+	WRectF ct;
+	ct.Top(ctRec.Top());
+	ct.Left(ctRec.Left());
+	ct.Bottom(ctRec.Bottom());
+	ct.Right(ctRec.Right());
+
+	if (!parent.IsColliding(ct))
+	{
+		return;
+	}
+
+	// Mask
+	ID2D1PathGeometry* MaskGeo;
+	WGraphicsContainer::Graphics()->GetFactory()->CreatePathGeometry(&MaskGeo);
+
+	// Geometry Sink
+	ID2D1GeometrySink* pSink = nullptr;
+	MaskGeo->Open(&pSink);
+	pSink->SetFillMode(D2D1_FILL_MODE_WINDING);
+	pSink->BeginFigure(D2D1::Point2F(ParentRect.left, ParentRect.top), D2D1_FIGURE_BEGIN_FILLED);
+	pSink->AddLine(D2D1::Point2F(ParentRect.right, ParentRect.top));
+	pSink->AddLine(D2D1::Point2F(ParentRect.right, ParentRect.bottom));
+	pSink->AddLine(D2D1::Point2F(ParentRect.left, ParentRect.bottom));
+	pSink->EndFigure(D2D1_FIGURE_END_CLOSED);
+	pSink->Close();
+
+	// Begin Mask Render
+	WGraphicsContainer::Graphics()->GetRenderTarget()->CreateLayer(nullptr, &maskLayer);
+	WGraphicsContainer::Graphics()->GetRenderTarget()->PushLayer(D2D1::LayerParameters(D2D1::InfiniteRect(), MaskGeo), maskLayer);
+
+	// Render Statements Go Here
+
+	int flushCounter = 0;
+	int flushPoint = 15;
+
+	// VERTICAL
+	int curWidthLoc = ctRec.Left();
+	while (curWidthLoc < ctRec.Right())
+	{
+		flushCounter++;
+		POINTF begin;
+		begin.x = (W_FLOAT)curWidthLoc;
+		begin.y = (W_FLOAT)ctRec.Top();
+
+		POINTF end;
+		end.x = (W_FLOAT)curWidthLoc;
+		end.y = (W_FLOAT)ctRec.Bottom();
+
+		WGraphicsContainer::Graphics()->DrawLine(begin, end, m_color, m_thickness);
+		if (flushCounter >= flushPoint)
+		{
+			WGraphicsContainer::Graphics()->SafeFlush();
+			flushCounter = 0;
+		}
+		curWidthLoc += m_sqSize;
+	}
+
+	// HORIZONTAL
+	int curHeightLoc = ctRec.Top();
+	while (curHeightLoc < ctRec.Bottom())
+	{
+		flushCounter++;
+
+		POINTF begin;
+		begin.x = (W_FLOAT)ctRec.Left();
+		begin.y = (W_FLOAT)curHeightLoc;
+
+		POINTF end;
+		end.x = (W_FLOAT)ctRec.Right();
+		end.y = (W_FLOAT)curHeightLoc;
+
+
+		WGraphicsContainer::Graphics()->DrawLine(begin, end, m_color, m_thickness);
+
+		if (flushCounter >= flushPoint)
+		{
+			WGraphicsContainer::Graphics()->SafeFlush();
+			flushCounter = 0;
+		}
+
+		curHeightLoc += m_sqSize;
+	}
+
+	// SQUARE
+	WGraphicsContainer::Graphics()->DrawRect(ctRec, m_thickness + 2, m_color, ctRec);
+	WGraphicsContainer::Graphics()->SafeFlush();
+
+
+	// End Mask Render
+	WGraphicsContainer::Graphics()->GetRenderTarget()->PopLayer();
+	SafeRelease(&maskLayer);
+	SafeRelease(&MaskGeo);
+	SafeRelease(&pSink);
+}
---
+++ src/Warp-Framework/WGridComponent.h
@@ -0,0 +1,34 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_GRID_COMPONENT_H_
+#define _W_GRID_COMPONENT_H_
+
+#include "WControl.h"
+#include "WLerp.h"
+
+class WGridComponent : public WControl
+{
+public:
+	WGridComponent(W_INT zIndex = 0);
+	WGridComponent(W_FLOAT top, W_FLOAT left, W_FLOAT bottom, W_FLOAT right, W_INT zIndex = 0);
+	WGridComponent(WPointF topleft, WPointF botright, W_INT zIndex = 0);
+	WGridComponent(WRectF location, W_INT zIndex = 0);
+	~WGridComponent(void) = default;
+
+	W_INT SquareSize(W_INT f);
+	W_FLOAT LineThickness(W_FLOAT f);
+	W_COLOR Color(W_COLOR col);
+
+	W_INT SquareSize(void) const;
+	W_FLOAT LineThickness(void) const;
+	W_COLOR Color(void) const;
+
+	void Render(void) override;
+
+protected:
+	W_INT m_sqSize;
+	W_FLOAT m_thickness;
+	W_COLOR m_color;
+};
+
+#endif // !_W_GRID_COMPONENT_H_
