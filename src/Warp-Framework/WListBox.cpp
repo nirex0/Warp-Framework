69a1d99c589337eaf0c4fc440fb186f3acbf9af7
Nirex0 <Nirex.0@gmail.com>
2018-07-20 21:42:22 +0430

    Update: WListBox now works with WRectF instead of WRECTF

--- src/Warp-Framework/WListBox.cpp
+++ src/Warp-Framework/WListBox.cpp
@@ -295,7 +295,7 @@ void WListBox::Render(void)
 	if (m_UseExtendedBorder)
 	{
 		// Top Bar
-		WRECTF TopBar = ctRec;
+		WRectF TopBar = ctRec;
 		TopBar.Top(ctRec.Top() - 5);
 		TopBar.Left(ctRec.Left() - 5);
 		TopBar.Bottom(ctRec.Top() - 4);
@@ -305,7 +305,7 @@ void WListBox::Render(void)
 		WGraphicsContainer::Graphics()->FillRectSolid(TopBar, bordColor);
 
 		// Left Bar
-		WRECTF LeftBar = ctRec;
+		WRectF LeftBar = ctRec;
 		LeftBar.Top(ctRec.Top() - 5);
 		LeftBar.Left(ctRec.Left() - 5);
 		LeftBar.Bottom(ctRec.Top() + GetHeight() / ExBordRatio + 10);
@@ -315,7 +315,7 @@ void WListBox::Render(void)
 		WGraphicsContainer::Graphics()->FillRectSolid(LeftBar, bordColor);
 
 		// Bottom Bar
-		WRECTF BottomBar = ctRec;
+		WRectF BottomBar = ctRec;
 
 		BottomBar.Top(ctRec.Bottom() + 4);
 		BottomBar.Left(ctRec.Right() - GetWidth() / ExBordRatio - 10);
@@ -326,7 +326,7 @@ void WListBox::Render(void)
 		WGraphicsContainer::Graphics()->FillRectSolid(BottomBar, bordColor);
 
 		// Right Bar
-		WRECTF RightBar = ctRec;
+		WRectF RightBar = ctRec;
 		RightBar.Top(ctRec.Bottom() - GetHeight() / ExBordRatio - 10);
 		RightBar.Left(ctRec.Right() + 4);
 		RightBar.Bottom(ctRec.Bottom() + 5);
