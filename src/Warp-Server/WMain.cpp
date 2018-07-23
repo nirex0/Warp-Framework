fd1ca210090bdf2cda625f52296f8ac2c9ff8ee4
Nirex0 <Nirex.0@gmail.com>
2018-07-24 04:07:16 +0430

    [Warp-Server] Add: WMain.cpp as the server's main entry point

---
+++ src/Warp-Server/WMain.cpp
@@ -0,0 +1,19 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WWin.h"
+#include "WDefines.h"
+
+W_INT WARP_ENTRY wWinMain(
+	HINSTANCE hInstance,			// UNUSED
+	HINSTANCE hPrevInstance,		// UNUSED
+	LPWSTR lpCmd,					// UNUSED
+	INT cmdShow						// UNUSED
+)
+{
+	UNREFERENCED_PARAMETER(hInstance);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmd);
+	UNREFERENCED_PARAMETER(cmdShow);
+
+	return 0;
+}
