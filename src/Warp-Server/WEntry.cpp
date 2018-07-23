02e6d9a5f288291e2c1a855db425aacc1b509858
Nirex0 <Nirex.0@gmail.com>
2018-07-24 03:59:30 +0430

    [Warp-Server] Add: WEntry class

---
+++ src/Warp-Server/WEntry.cpp
@@ -0,0 +1,12 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WEntry.h"
+
+WEntry::WEntry()
+{
+}
+
+
+WEntry::~WEntry()
+{
+}
---
+++ src/Warp-Server/WEntry.h
@@ -0,0 +1,14 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_ENTRY_H_
+#define _W_ENTRY_H_
+
+class WEntry
+{
+public:
+	WEntry();
+	~WEntry();
+};
+
+#endif // !_W_ENTRY_H_
+
