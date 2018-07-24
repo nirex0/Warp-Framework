59324ff205f9f7b74f5c89469f2b75e0b4c2cec2
Nirex0 <Nirex.0@gmail.com>
2018-07-25 00:28:54 +0430

    [Warp-Server] Add: WSAsyncArgs Class

---
+++ src/Warp-Server/WSAsyncArgs.cpp
@@ -0,0 +1,12 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#include "WSAsyncArgs.h"
+
+WSAsyncArgs::WSAsyncArgs()
+{
+}
+
+
+WSAsyncArgs::~WSAsyncArgs()
+{
+}
---
+++ src/Warp-Server/WSAsyncArgs.h
@@ -0,0 +1,13 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _WS_ASYNC_ARGS_H_
+#define _WS_ASYNC_ARGS_H_
+
+class WSAsyncArgs
+{
+public:
+	WSAsyncArgs();
+	~WSAsyncArgs();
+};
+
+#endif // !_WS_ASYNC_ARGS_H_
