987846f6305637d1daf41f8ec5b31bf258284de8
Nirex0 <Nirex.0@gmail.com>
2018-07-25 00:25:13 +0430

    [Warp-Server] Change: WWin to WSWin

--- src/Warp-Server/WSWin.h
+++ src/Warp-Server/WSWin.h
@@ -1,7 +1,7 @@
 // © 2018 NIREX ALL RIGHTS RESERVED
 
-#ifndef _W_WIN_H_
-#define _W_WIN_H_
+#ifndef _WS_WIN_H_
+#define _WS_WIN_H_
 
 #ifndef _STD_WINDOWS_H_
 #define _STD_WINDOWS_H_
@@ -50,5 +50,5 @@
 #include <Windows.h>
 #endif // !_STD_WINDOWS_H_
 
-#endif // !_W_WIN_H_
+#endif // !_WS_WIN_H_
 
