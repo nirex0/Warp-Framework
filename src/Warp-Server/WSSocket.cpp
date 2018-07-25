71f23210296e54cc99eb49de640d75d529c62893
Nirex0 <Nirex.0@gmail.com>
2018-07-26 02:35:44 +0430

    [Warp-Server] Update: Implemented WSSocket

--- src/Warp-Server/WSSocket.cpp
+++ src/Warp-Server/WSSocket.cpp
@@ -2,10 +2,121 @@
 
 #include "WSSocket.h"
 
-WSSocket::WSSocket()
+WSSocket::WSSocket(std::string ip, int port, int bufferSize)
+	: m_ip(ip)
+	, m_port(port)
+	, m_bufferSize(bufferSize)
 {
+	m_buffer = new char[m_bufferSize];
+	WORD version = MAKEWORD(2, 2);
+	WSAStartup(version,&m_data);
+	m_sock = socket(AF_INET, SOCK_STREAM, 0);
 }
 
-WSSocket::~WSSocket()
+WSSocket::~WSSocket(void)
 {
+	CleanUp();
+}
+
+W_INT WSSocket::Bind(void)
+{
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(m_port);
+	inet_pton(AF_INET, m_ip.c_str(), &hint.sin_addr);
+
+	return bind(m_sock, (sockaddr*)&hint, sizeof(hint));
+}
+
+W_INT WSSocket::Listen(W_INT backLog)
+{
+	return listen(m_sock, backLog);
+}
+
+W_INT WSSocket::Accept(SOCKET clientSocket, int& outClientSize)
+{
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	clientSocket = accept(m_sock, (sockaddr*)&client, &clientSize);
+
+	outClientSize = clientSize;
+
+	return (clientSocket == INVALID_SOCKET);
+}
+
+W_INT WSSocket::Connect(void)
+{
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(m_port);
+	inet_pton(AF_INET, m_ip.c_str(), &hint.sin_addr);
+
+	int connResult = connect(m_sock, (sockaddr*)&hint, sizeof(hint));
+	if (connResult == SOCKET_ERROR)
+	{
+		CleanUp();
+		return SOCKET_ERROR;
+	}
+}
+
+W_INT WSSocket::Send(SOCKET fromSocket, char* inData, int dataLength)
+{
+	return send(fromSocket, inData, dataLength, 0);
+}
+
+W_INT WSSocket::Receive(SOCKET fromSocket, char* outData, int& outLength)
+{
+	int bytes = recv(fromSocket, m_buffer, m_bufferSize, 0);
+	
+	outData = m_buffer;
+	outLength = bytes;
+
+	if (bytes == SOCKET_ERROR)
+	{
+		CleanUp();
+		return SOCKET_ERROR;
+	}
+	if (bytes == 0)
+	{
+		CleanUp();
+		return NULL;
+	}
+
+	for (size_t i = 0; i < bytes; i++)
+	{
+		m_cleanedData += m_buffer[i];
+	}
+
+	return 0;
+}
+
+SOCKET WSSocket::Socket(void) const
+{
+	return m_sock;
+}
+
+WSADATA WSSocket::SocketData(void) const
+{
+	return m_data;
+}
+
+W_INT WSSocket::Port(void) const
+{
+	return m_port;
+}
+
+std::string WSSocket::IP(void) const
+{
+	return m_ip;
+}
+
+std::string WSSocket::Data(void) const
+{
+	return m_cleanedData;
+}
+
+W_INT WSSocket::CleanUp(void)
+{
+	return WSACleanup();
 }
--- src/Warp-Server/WSSocket.h
+++ src/Warp-Server/WSSocket.h
@@ -3,11 +3,45 @@
 #ifndef _WS_SOCKET_H_
 #define _WS_SOCKET_H_
 
+#include <string>
+
+#include "WSSock.h"
+#include "WSDefines.h"
+
 class WSSocket
 {
 public:
-	WSSocket();
-	~WSSocket();
+	WSSocket(std::string ip, int port, int bufferSize);
+	~WSSocket(void);
+
+	W_INT Bind(void);
+	W_INT Listen(W_INT backLog);
+
+	W_INT Accept(SOCKET clientSocket, int& outClientSize);
+	W_INT Connect(void);
+
+	W_INT Send(SOCKET fromSocket, char* inData, int dataLength);
+	W_INT Receive(SOCKET fromSocket, char* outData, int& outLength);
+
+	SOCKET Socket(void) const;
+	WSADATA SocketData(void)const;
+
+	W_INT Port(void) const;
+	std::string IP(void) const;
+	std::string Data(void) const;
+
+private:
+	W_INT CleanUp(void);
+
+	W_INT m_port;
+	std::string m_ip;
+
+	std::string m_cleanedData;
+
+	WSADATA m_data;
+	SOCKET m_sock;
+	char* m_buffer;
+	W_INT m_bufferSize;
 };
 
 #endif // !_WS_SOCKET_H_
