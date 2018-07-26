d92edf39d6e5c3bfa80e8d09b639a1850d679c5d
Nirex0 <Nirex.0@gmail.com>
2018-07-26 21:17:27 +0430

    [Warp-Server] Add: WSRegistry class

--- src/Warp-Server/WSRegistry.h
+++ src/Warp-Server/WSRegistry.h
@@ -3,11 +3,34 @@
 #ifndef _WS_REGISTRY_H_
 #define _WS_REGISTRY_H_
 
-class WSRegistry
+#include "WSEntity.h"
+#include "WSEventArgs.h"
+
+#include <vector>
+#include <functional>
+
+typedef std::function<void(WSEntity* sender, WSEventArgs* args)> Annex;
+
+class WSRegistry final : public WSEntity
 {
 public:
 	WSRegistry();
+	WSRegistry(const WSRegistry& nRegistery);
+	WSRegistry(WSRegistry&& nRegistery);
 	~WSRegistry();
+
+	long GetCount(void) const;
+
+	WSRegistry* operator()(WSEntity* sender, WSEventArgs* args);
+	WSRegistry* Run(WSEntity* sender, WSEventArgs* args);
+	WSRegistry* Register(Annex rhs);
+	WSRegistry* UnRegister(Annex* rhs);
+	WSRegistry* operator+=(Annex rhs);
+	WSRegistry* operator-=(Annex rhs);
+
+private:
+	volatile long m_count;
+	std::vector<Annex*> m_registered;
 };
 
 #endif // !_WS_REGISTRY_H_
