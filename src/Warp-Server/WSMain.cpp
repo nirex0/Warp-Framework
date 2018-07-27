ac1bdfcab6bad629c497cf82788d5246d534604a
Nirex0 <Nirex.0@gmail.com>
2018-07-27 10:35:22 +0430

    [Warp-Server] Update: WSMain is now (almost) fully implemented

--- src/Warp-Server/WSMain.cpp
+++ src/Warp-Server/WSMain.cpp
@@ -4,20 +4,37 @@
 #include "WSDefines.h"
 #include "WSContainer.h"
 
+#include <memory>
+
 // Initializing WSContainer members
 WSEntry* WSContainer::EntryServer = {};
 
+#ifndef CONSOLE
 W_INT WARP_ENTRY wWinMain(
 	HINSTANCE hInstance,			// UNUSED
 	HINSTANCE hPrevInstance,		// UNUSED
 	LPWSTR lpCmd,					// UNUSED
 	INT cmdShow						// UNUSED
 )
+#endif // !CONSOLE
+#ifdef CONSOLE
+W_INT main(int argc, char** argv)
+#endif //CONSOLE
 {
+#ifndef CONSOLE
 	UNREFERENCED_PARAMETER(hInstance);
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmd);
 	UNREFERENCED_PARAMETER(cmdShow);
+#endif // !CONSOLE
+	std::unique_ptr<WSEntry> mainEntry = std::make_unique<WSEntry>(WSEntry());
+	WSContainer::App(mainEntry.get());
+	
+	mainEntry->Start();
+	while (true)
+	{
+		mainEntry->Update();
+	}
 
 	return 0;
 }
