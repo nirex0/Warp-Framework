75458ebbf48fc98708cd8d860de8d3019b6140f9
Nirex0 <Nirex.0@gmail.com>
2018-07-29 23:58:01 +0430

    [Warp-Framework] Update: WSafeRelease now does a better job at Error Handling

--- src/Warp-Framework/WSafeRelease.h
+++ src/Warp-Framework/WSafeRelease.h
@@ -3,14 +3,18 @@
 #ifndef _W_SAFE_RELEASE_H_
 #define _W_SAFE_RELEASE_H_
 
+#include "WWin.h"
+
 template <class T> 
-void SafeRelease(T** ppT)
+HRESULT SafeRelease(T** ppResource)
 {
-    if (*ppT)
+    if (*ppResource)
     {
-        (*ppT)->Release();
-        *ppT = nullptr;
+        (*ppResource)->Release();
+        *ppResource = nullptr;
+		return S_OK;
     }
+	return E_ABORT;
 }
 
 #endif // !_W_SAFE_RELEASE_H_
