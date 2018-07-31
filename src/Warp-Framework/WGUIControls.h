cbf58928fa42d020f9e40ad75548d129036f7cca
Nirex0 <Nirex.0@gmail.com>
2018-08-01 02:32:59 +0430

    [Warp-Framework] Add: Global header for Control classes

---
+++ src/Warp-Framework/WGUIControls.h
@@ -0,0 +1,21 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_GUI_CONTROLS_H_
+#define _W_GUI_CONTROLS_H_
+
+#include "WControl.h"
+#include "WButton.h"
+#include "WCheckBox.h"
+#include "WFixedCanvas.h"
+#include "WImageBox.h"
+#include "WLabel.h"
+#include "WListBox.h"
+#include "WListBoxItem.h"
+#include "WProgressBar.h"
+#include "WRadioButton.h"
+#include "WScrollView.h"
+#include "WSeekBar.h"
+#include "WTextBox.h"
+#include "WToggleButton.h"
+
+#endif // !_W_GUI_CONTROLS_H_
