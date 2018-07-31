ad6ba297693fc375fe404c803186a3a52e0d1ca1
Nirex0 <Nirex.0@gmail.com>
2018-08-01 02:33:16 +0430

    [Warp-Framework] Add: Global header for Async classes

---
+++ src/Warp-Framework/WAsync.h
@@ -0,0 +1,15 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_ASYNC_H_
+#define _W_ASYNC_H_
+
+#include "WColorTransform.h"
+#include "WColorTransformArgs.h"
+#include "WLerp.h"
+#include "WLerpArgs.h"
+#include "WSmoothStep.h"
+#include "WSmoothStepArgs.h"
+#include "WAsyncWorker.h"
+#include "WAsyncArgs.h"
+
+#endif // !_W_ASYNC_H_
