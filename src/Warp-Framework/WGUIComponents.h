9fc22d6c6e7b265d857df84f85b5cc6d1c28d058
Nirex0 <Nirex.0@gmail.com>
2018-08-01 02:32:51 +0430

    [Warp-Framework] Add: Global header for Component classes

---
+++ src/Warp-Framework/WGUIComponents.h
@@ -0,0 +1,10 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_GUI_COMPONENTS_H_
+#define _W_GUI_COMPONENTS_H_
+
+#include "WGridComponent.h"
+#include "WCGridComponent.h"
+#include "WExtendedBorderComponent.h"
+
+#endif // !_W_GUI_CONTROLS_H_
