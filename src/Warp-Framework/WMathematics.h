875f2481a8af11ceea68c71d5c7cdd3a3d018257
Nirex0 <Nirex.0@gmail.com>
2018-08-02 01:10:21 +0430

    [Warp-Framework] Add: WMathematics "Wrapper" header

---
+++ src/Warp-Framework/WMathematics.h
@@ -0,0 +1,13 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_MATHEMATICS_H_
+#define _W_MATHEMATICS_H_
+
+#include "WMath.h"
+#include "WMatrix2.h"
+#include "WMatrix3.h"
+#include "WVector2.h"
+#include "WVector3.h"
+#include "WVector4.h"
+
+#endif // !_W_MATHEMATICS_H_
