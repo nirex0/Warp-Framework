2f724ca43abca5d6862679709d4c86d2700cafa5
Nirex0 <Nirex.0@gmail.com>
2018-08-02 01:09:19 +0430

    [Warp-Framework] Add: WDevices "Wrapper" header

---
+++ src/Warp-Framework/WDevices.h
@@ -0,0 +1,11 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_DEVICES_H_
+#define _W_DEVICES_H_
+
+#include "WMouse.h"
+#include "WMouseArgs.h"
+#include "WKeyboard.h"
+#include "WKeyboardArgs.h"
+
+#endif // !_W_DEVICES_H_
