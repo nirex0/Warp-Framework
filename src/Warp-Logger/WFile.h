d8327b5cc5f94ae0472d226468bde2b393d5f12e
Nirex0 <Nirex.0@gmail.com>
2018-08-07 02:18:18 +0430

    [Warp-Logger] Add: WFile Utility class

---
+++ src/Warp-Logger/WFile.h
@@ -0,0 +1,25 @@
+//© 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _W_FILE_H_
+#define _W_FILE_H_
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <iostream>
+
+class WFile final
+{
+public:
+	std::string ReadAllText(std::string file);
+	int WriteAllText(std::string file, std::string str);
+	std::int32_t FindFirst(std::string file, std::string val);
+
+	std::vector<std::string> ReadAllLines(std::string file);
+	int WriteAllLines(std::string file, std::vector<std::string> vecstr);
+	std::vector<int32_t> FindAll(std::string file, std::string val);
+
+	bool SafeFread(void* buffer, int size, int number, FILE* fp);
+};
+
+#endif // !_W_FILE_H_
