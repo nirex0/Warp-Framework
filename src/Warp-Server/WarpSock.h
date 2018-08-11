81240be6f58cde14ff494ca8e3d881340b99da79
Nirex0 <Nirex.0@gmail.com>
2018-08-11 10:43:45 +0430

    [Warp-Server] Change: WSSocket to WSsocket to avoid confliction

--- src/Warp-Server/WarpSock.h
+++ src/Warp-Server/WarpSock.h
@@ -6,13 +6,13 @@
 #include <WS2tcpip.h>
 #pragma comment (lib, "ws2_32.lib")
 
-typedef SOCKET WSSocket;
+typedef SOCKET WSsocket;
 
 #define WARP_FRAMEWORK
 #define W_SERVER
 #define WARP
 
-#define V4 AF_INET
-#define V6 AF_INET6
+#define IPV4 AF_INET
+#define IPV6 AF_INET6
 
 #endif // !_WS_WARP_SOCK_H_
