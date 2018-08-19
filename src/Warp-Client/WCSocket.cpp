10eff389e85ed164c7447c012d4534d1d46d61b1
Nirex0 <Nirex.0@gmail.com>
2018-08-19 11:47:21 +0430

    [Warp-Client] Add: WCSocket class (Empty)

---
+++ src/Warp-Client/WCSocket.cpp
@@ -0,0 +1,12 @@
+#include "WCSocket.h"
+
+
+
+WCSocket::WCSocket()
+{
+}
+
+
+WCSocket::~WCSocket()
+{
+}
---
+++ src/Warp-Client/WCSocket.h
@@ -0,0 +1,8 @@
+#pragma once
+class WCSocket
+{
+public:
+	WCSocket();
+	~WCSocket();
+};
+
