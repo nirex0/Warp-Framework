9c1f75cc9e6d76e3212452ac3bd10f22c717e2b9
Nirex0 <Nirex.0@gmail.com>
2018-08-19 11:47:28 +0430

    [Warp-Client] Add: WCSocketArgs class (Empty)

---
+++ src/Warp-Client/WCSocketArgs.cpp
@@ -0,0 +1,12 @@
+#include "WCSocketArgs.h"
+
+
+
+WCSocketArgs::WCSocketArgs()
+{
+}
+
+
+WCSocketArgs::~WCSocketArgs()
+{
+}
---
+++ src/Warp-Client/WCSocketArgs.h
@@ -0,0 +1,8 @@
+#pragma once
+class WCSocketArgs
+{
+public:
+	WCSocketArgs();
+	~WCSocketArgs();
+};
+
