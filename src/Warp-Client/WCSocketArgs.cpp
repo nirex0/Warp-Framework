b05f6ef495485263a904383b7fef5af313aebb32
Nirex0 <Nirex.0@gmail.com>
2018-08-20 17:35:40 +0430

    [Warp-Client] Update: WCSocketArgs class is now fully implemented

--- src/Warp-Client/WCSocketArgs.cpp
+++ src/Warp-Client/WCSocketArgs.cpp
@@ -1,12 +1,29 @@
+// © 2018 NIREX ALL RIGHTS RESERVED
+
 #include "WCSocketArgs.h"
 
-
-
-WCSocketArgs::WCSocketArgs()
+WCSocketArgs::WCSocketArgs(SOCKET sock, char* data, int len)
+	: m_sock(sock)
+	, m_data(data)
+	, m_len(len)
 {
 }
 
-
 WCSocketArgs::~WCSocketArgs()
 {
+}
+
+SOCKET WCSocketArgs::Socket(void) const
+{
+	return m_sock;
+}
+
+char* WCSocketArgs::Data(void) const
+{
+	return m_data;
+}
+
+int WCSocketArgs::Length(void) const
+{
+	return m_len;
 }
--- src/Warp-Client/WCSocketArgs.h
+++ src/Warp-Client/WCSocketArgs.h
@@ -1,8 +1,25 @@
-#pragma once
-class WCSocketArgs
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _WC_SOCKET_ARGS_H_
+#define _WC_SOCKET_ARGS_H_
+
+#include "WCEventArgs.h"
+#include "WCSock.h"
+
+class WCSocketArgs : public WCEventArgs
 {
 public:
-	WCSocketArgs();
-	~WCSocketArgs();
+	WCSocketArgs(SOCKET sock, char* data, int len);
+	~WCSocketArgs(void);
+
+	SOCKET Socket(void) const;
+	char* Data(void) const;
+	int Length(void) const;
+
+private:
+	SOCKET m_sock;
+	char* m_data;
+	int m_len;
 };
 
+#endif // !_WC_SOCKET_ARGS_H_
