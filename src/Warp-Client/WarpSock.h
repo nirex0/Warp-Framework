24eca007e3148c45b2c6acdd269db722b7e8fea0
Nirex0 <Nirex.0@gmail.com>
2018-08-20 17:31:55 +0430

    [Warp-Client] Update: WarpSock.h is now implemented

--- src/Warp-Client/WarpSock.h
+++ src/Warp-Client/WarpSock.h
@@ -1,1 +1,19 @@
-#pragma once
+// © 2018 NIREX ALL RIGHTS RESERVED
+
+#ifndef _WC_WARP_SOCK_H_
+#define _WC_WARP_SOCK_H_
+
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#pragma comment (lib, "ws2_32.lib")
+
+typedef SOCKET WCsocket;
+
+#define WARP_FRAMEWORK
+#define W_CLIENT
+#define WARP
+
+#define IPV4 AF_INET
+#define IPV6 AF_INET6
+
+#endif // !_WC_WARP_SOCK_H_
