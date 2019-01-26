1b86b0c88ddd62038c9b787efd89fc6a867fd3cf
Nirex0 <Nirex.0@gmail.com>
2019-01-26 17:36:20 +0330

    Update: WColorTransformArgs.h is now ready for auto documentation

--- src/Warp-Framework/WColorTransformArgs.h
+++ src/Warp-Framework/WColorTransformArgs.h
@@ -7,13 +7,22 @@
 #include "WEventArgs.h"
 #include "WDX.h"
 
+//-->DOC_CLASS
+// Arguments passed through the WColorTransform Registry classes
 class WColorTransformArgs : public WEventArgs
 {
 public:
+	//-->DOC_FUNC
+	// col: Argument value to be passed through the WColorTransform Registry
+	// Constructor of the WColorTransformArgs class
 	WColorTransformArgs(W_COLOR col);
+	//-->DOC_FUNC
+	// Value Getter function
 	W_COLOR Value(void) const;
 
 private:
+	//-->DOC_MEMBER
+	// Value of the color to be sent through the WColorTransform Registry
 	W_COLOR	m_Value;
 };
 
