9bc90c3179ce26eb5c34bd610d48d60aaf23d0d1
Nirex0 <Nirex.0@gmail.com>
2019-01-26 18:02:55 +0330

    Docreator-Update: WLerpArgs is now ready for auto documentation

--- src/Warp-Framework/WLerpArgs.h
+++ src/Warp-Framework/WLerpArgs.h
@@ -6,14 +6,27 @@
 #include "WDefines.h"
 #include "WEventArgs.h"
 
+//-->DOC_CLASS
+// Arguments passed through the WLerp Registry classes
 class WLerpArgs : public WEventArgs
 {
 public:
+	//-->DOC_FUNC
+	// val: Argument value to be passed through the WLerp Registry
+	// Constructor of the WLerpArgs class
 	WLerpArgs(W_FLOAT val);
+
+	//-->DOC_FUNC
+	// Value Getter function (With Precision)
 	W_FLOAT ValueExact(void) const;
+
+	//-->DOC_FUNC
+	// Value Getter function (Rounded)
 	W_INT ValueRounded(void) const;
 
 private:
+	//-->DOC_MEMBER
+	// Value to be sent through the WLerp Registry
 	W_FLOAT	m_Value;
 };
 
