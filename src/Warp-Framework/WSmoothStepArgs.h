fe12f8909fd509d4f9b3e122749d47f33cab75f7
Nirex0 <Nirex.0@gmail.com>
2019-01-26 18:25:02 +0330

    Update: WSmoothStepArgs.h is now ready for docreator

--- src/Warp-Framework/WSmoothStepArgs.h
+++ src/Warp-Framework/WSmoothStepArgs.h
@@ -6,14 +6,27 @@
 #include "WDefines.h"
 #include "WEventArgs.h"
 
+//-->DOC_CLASS
+// Arguments passed through the WSmoothStep Registry classes
 class WSmoothStepArgs : public WEventArgs
 {
-public:
+public:	
+	//-->DOC_FUNC
+	// val: Argument value to be passed through the WSmoothStep Registry
+	// Constructor of the WSmoothStepArgs class
 	WSmoothStepArgs(W_FLOAT val);
+
+	//-->DOC_FUNC
+	// Value Getter function (With Precision)
 	W_FLOAT ValueExact(void) const;
+
+	//-->DOC_FUNC
+	// Value Getter function (Rounded)
 	W_INT ValueRounded(void) const;
 
 private:
+	//-->DOC_MEMBER
+	// Value to be sent through the WSmoothStep Registry
 	W_FLOAT	m_Value;
 };
 
