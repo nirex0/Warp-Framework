156b1d5d44fc087ec8f0a329d486b3eaeeb522ad
Nirex0 <Nirex.0@gmail.com>
2019-01-26 18:03:01 +0330

    Docreator-Update: WLerp is now ready for auto documentation

--- src/Warp-Framework/WLerp.h
+++ src/Warp-Framework/WLerp.h
@@ -14,13 +14,20 @@
 #include <thread>
 #include <future>
 
+//-->DOC_CLASS
+// A Class for performing a smooth Linear Interpolation (LERP) transformation between two numbers
 class WLerp : public WEntity
 {
 public:
+	//-->DOC_FUNC
+	// Initialize all the required values to perform a LERP transformation
+	// From: color value to begin with
+	// To: target color value
+	// Alpha: Alpha value of the LERP
+	// Delay: Delay between each LERP epoch
 	WLerp(W_FLOAT from, W_FLOAT to, W_FLOAT alpha, W_LONG delay = 1);
-	~WLerp();
+	~WLerp(void);
 
-// Getters
 	WRegistry* TickRegistry(void) const;
 	WRegistry* DoneRegistry(void) const;
 
@@ -29,9 +36,12 @@ public:
 	W_FLOAT From(void) const;
 	W_FLOAT To(void) const;
 	W_FLOAT Alpha(void) const;
+	
+	//-->DOC_FUNC
+	// Function to determine whether or not the Transformation is being performed
+	// Returns: True if in transform, False if transform is finished. 
 	bool IsRunning(void);
 
-// Setters
 	WRegistry* TickRegistry(WRegistry* intake);
 	WRegistry* DoneRegistry(WRegistry* intake);
 
@@ -41,9 +51,16 @@ public:
 	W_FLOAT To(W_FLOAT intake);
 	W_FLOAT Alpha(W_FLOAT intake);
 
-// Functions
+	//-->DOC_FUNC
+	// Begin Transformation (no safeguards)
 	void Perform(void);
+
+	//-->DOC_FUNC
+	// Begin Transformation (with safeguards)
 	void PerformSafe(void);
+
+	//-->DOC_FUNC
+	// Stop Transformation if it's already happening
 	void Stop(void);
 
 private:
@@ -51,10 +68,24 @@ private:
 	bool WorkThread(std::thread& out);
 
 private:
+	//-->DOC_MEMBER
+	// Delay Between each epoch
 	W_LONG m_delay;
+
+	//-->DOC_MEMBER
+	// Current Value of the LERP
 	W_FLOAT m_value;
+
+	//-->DOC_MEMBER
+	// Value to begin with
 	W_FLOAT m_from;
+
+	//-->DOC_MEMBER
+	// Target value to get close to
 	W_FLOAT m_to;
+
+	//-->DOC_MEMBER
+	// Alpha Value of LERP
 	W_FLOAT m_alpha;
 
 	std::thread* thr;
@@ -63,7 +94,12 @@ private:
 	bool m_stop;
 	bool m_isRunning;
 
+	//-->DOC_MEMBER
+	// Function registry that is called on each Tick
 	WRegistry* m_LerpTickRegistry;
+
+	//-->DOC_MEMBER
+	// Function registry that is called when transformation is finished
 	WRegistry* m_LerpDoneRegistry;
 };
 
