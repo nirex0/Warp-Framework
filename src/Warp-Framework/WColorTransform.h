9f3bfa19937eb2542eafe8a62a7807a54fab3454
Nirex0 <Nirex.0@gmail.com>
2019-01-26 17:36:12 +0330

    Update: WColorTransform.h is now ready for auto documentation

--- src/Warp-Framework/WColorTransform.h
+++ src/Warp-Framework/WColorTransform.h
@@ -15,13 +15,20 @@
 #include <thread>
 #include <future>
 
+//-->DOC_CLASS
+// A Class for performing a smooth Linear Interpolation (LERP) color transformation between two colors
 class WColorTransform : public WEntity
 {
 public:
-	WColorTransform(W_COLOR From, W_COLOR To, W_FLOAT alpha, W_LONG Delay);
-	~WColorTransform();
+	//-->DOC_FUNC
+	// Initialize all the required values to perform a color transformation
+	// From: color value to begin with
+	// To: target color value
+	// Alpha: Alpha value of the LERP
+	// Delay: Delay between each LERP epoch
+	WColorTransform(W_COLOR From, W_COLOR To, W_FLOAT Alpha, W_LONG Delay);
+	~WColorTransform(void);
 
-	// Getters
 	WRegistry* TickRegistry(void) const;
 	WRegistry* DoneRegistry(void) const;
 
@@ -30,9 +37,12 @@ public:
 	W_COLOR Value(void) const;
 	W_COLOR From(void) const;
 	W_COLOR To(void) const;
+
+	//-->DOC_FUNC
+	// Function to determine whether or not the Transformation is being performed
+	// Returns: True if in transform, False if transform is finished. 
 	bool IsRunning(void);
 
-	// Setters
 	WRegistry* TickRegistry(WRegistry* intake);
 	WRegistry* DoneRegistry(WRegistry* intake);
 
@@ -42,9 +52,16 @@ public:
 	W_COLOR From(W_COLOR intake);
 	W_COLOR To(W_COLOR intake);
 	
-// Functions
+	//-->DOC_FUNC
+	// Begin Transformation (no safeguards)
 	void Perform(void);
+
+	//-->DOC_FUNC
+	// Begin Transformation (with safeguards)
 	void PerformSafe(void);
+
+	//-->DOC_FUNC
+	// Stop Transformation if it's already happening
 	void Stop(void);
 
 private:
@@ -52,10 +69,24 @@ private:
 	bool WorkThread(std::thread& out);
 
 private:
+	//-->DOC_MEMBER
+	// Delay Between each epoch
 	W_LONG m_delay;
+
+	//-->DOC_MEMBER
+	// Alpha Value of LERP
 	W_FLOAT m_alpha;
+
+	//-->DOC_MEMBER
+	// Current Value of the LERP
 	W_COLOR m_value;
+
+	//-->DOC_MEMBER
+	// Value to begin with
 	W_COLOR m_from;
+
+	//-->DOC_MEMBER
+	// Target value to get close to
 	W_COLOR m_to;
 
 	std::thread* thr;
@@ -64,7 +95,12 @@ private:
 	bool m_stop;
 	bool m_isRunning;
 
+	//-->DOC_MEMBER
+	// Function registry that is called on each Tick
 	WRegistry* m_CTTickRegistry;
+
+	//-->DOC_MEMBER
+	// Function registry that is called when transformation is finished
 	WRegistry* m_CTDoneRegistry;
 };
 
