6d248c7728dd6ee29af7670dcd71c53bc54c60e6
Nirex0 <Nirex.0@gmail.com>
2019-01-28 22:58:57 +0330

    Update: WContainer.h is now ready for docreator

--- src/Warp-Framework/WContainer.h
+++ src/Warp-Framework/WContainer.h
@@ -10,6 +10,8 @@
 #include "WKeyboard.h"
 #include "WTheme.h"
 
+//-->DOC_CLASS
+// A Container class that allows access to a set of global static variables throughout the framework
 class WContainer final
 {
 public:
@@ -71,24 +73,80 @@ public:
 	static WTheme Theme(void);
 
 private:
+	//-->DOC_MEMBER
+	//Holds the HRESULT of the last done operation
 	static HRESULT HR;
+
+	//-->DOC_MEMBER
+	//A Handle to the main window of the framework
 	static HWND hWnd;
+
+	//-->DOC_MEMBER
+	//Last Sent Message through the windows message loop
 	static UINT msg;
+
+	//-->DOC_MEMBER
+	//WParam of the windows message loop
 	static WPARAM wParam;
+
+	//-->DOC_MEMBER
+	//LParam of the windows message loop
 	static LPARAM lParam;
+
+	//-->DOC_MEMBER
+	//Entry Class
 	static WEntry WFramework;
+
+	//-->DOC_MEMBER
+	//Current Delta Time of the render engine
 	static DELTATIME DeltaTime;
+
+	//-->DOC_MEMBER
+	//Background alpha value
 	static W_INT BGA;
+
+	//-->DOC_MEMBER
+	//Background red value
 	static W_INT BGR;
+
+	//-->DOC_MEMBER
+	//Background green value
 	static W_INT BGG;
+
+	//-->DOC_MEMBER
+	//Background blue value
 	static W_INT BGB;
+
+	//-->DOC_MEMBER
+	//Current Window Width
 	static W_INT W_WIDTH;
+
+	//-->DOC_MEMBER
+	//Current Window Height
 	static W_INT W_HEIGHT;
+
+	//-->DOC_MEMBER
+	//Pointer to the mouse object
 	static WMouse* mouse;
+
+	//-->DOC_MEMBER
+	//Pointer to the keyboard object
 	static WKeyboard* keyboard;
+
+	//-->DOC_MEMBER
+	//RESERVED FOR DRAGMOVE WINDOW MOVEMENT
 	static W_INT helperCoordX;
+
+	//-->DOC_MEMBER
+	//RESERVED FOR DRAGMOVE WINDOW MOVEMENT
 	static W_INT helperCoordY;
+	
+	//-->DOC_MEMBER
+	//Holder for whether or not window is in a DragMove State
 	static BOOL bDragMove;
+
+	//-->DOC_MEMBER
+	//Theme of the framework
 	static WTheme wTheme;
 };
 
