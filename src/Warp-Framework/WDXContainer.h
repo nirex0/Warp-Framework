42c3ab23b3801002f33ad1dc085cd9fc9741cc86
Nirex0 <Nirex.0@gmail.com>
2019-01-28 22:59:02 +0330

    Update: WDXContainer.h is now ready for docreator

--- src/Warp-Framework/WDXContainer.h
+++ src/Warp-Framework/WDXContainer.h
@@ -6,6 +6,9 @@
 #include "WDX.h"
 
 #ifdef _WDX_
+
+//-->DOC_CLASS
+// A Container class that allows access to a set of global static DirectX and DirectWrite variables throughout the framework
 class WDXContainer final
 {
 public:
@@ -25,10 +28,24 @@ public:
 	static const IDWriteTextFormat* TextFormat(void);
 
 private:
+	//-->DOC_MEMBER
+	//Pointer to the main Direct2D Factory
 	static ID2D1Factory* DX_Factory;
+
+	//-->DOC_MEMBER
+	//Pointer to the main Direct2D Render Target
 	static ID2D1HwndRenderTarget* DX_RT;
+
+	//-->DOC_MEMBER
+	//Window Rect
 	static RECT DX_cRect;
+
+	//-->DOC_MEMBER
+	//Pointer to the main DirectWrite Factory
 	static IDWriteFactory* DX_WFactory;
+
+	//-->DOC_MEMBER
+	//Pointer to the main DirectWrite TextFormat
 	static IDWriteTextFormat* DX_TextFormat;
 };
 
