cc49e9d7050f247b41bcd556af68691d25297c80
Nirex0 <Nirex.0@gmail.com>
2019-01-28 22:59:20 +0330

    Update: WRegContainer.h is now ready for docreator

--- src/Warp-Framework/WRegContainer.h
+++ src/Warp-Framework/WRegContainer.h
@@ -6,10 +6,11 @@
 #include "WRegistry.h"
 #include "WUniqueRegister.h"
 
+//-->DOC_CLASS
+// A Container class that allows access to a set of global static registry pointers throughout the framework
 class WRegContainer final
 {
 public:
-// Keyboard
 	static void KeyboardKeyDown(WUniqueRegister* const kbdReg);
 	static WUniqueRegister* KeyboardKeyDown(void);
 
@@ -19,7 +20,6 @@ public:
 	static void KeyboardOnChar(WUniqueRegister* const kbdReg);
 	static WUniqueRegister* KeyboardOnChar(void);
 
-// MOUSE
 	static void MouseDown(WRegistry* const mouseReg);
 	static WRegistry* MouseDown(void);
 
@@ -35,21 +35,44 @@ public:
 	static void MouseRollDown(WRegistry* const mouseReg);
 	static WRegistry* MouseRollDown(void);
 
-// MAIN WINDOW
 	static void OnGDIPaint(WUniqueRegister* const gdiReg);
 	static WUniqueRegister* OnGDIPaint(void);
 
 private:
+	//-->DOC_MEMBER
+	//Keyboard Key down Registry pointer
 	static WUniqueRegister* KBD_KeyDownReg;
+
+	//-->DOC_MEMBER
+	//Keyboard Key up Registry pointer
 	static WUniqueRegister* KBD_KeyUpReg;
+
+	//-->DOC_MEMBER
+	//Keyboard On Char Registry pointer (For Typing)
 	static WUniqueRegister* KBD_OnCharReg;
 
+	//-->DOC_MEMBER
+	//Mouse On Click down Registry pointer
 	static WRegistry* MOS_MouseDown;
+
+	//-->DOC_MEMBER
+	//Mouse On Click up Registry pointer
 	static WRegistry* MOS_MouseUp;
+
+	//-->DOC_MEMBER
+	//Mouse On Mouse movement Registry pointer
 	static WRegistry* MOS_MouseMove;
+
+	//-->DOC_MEMBER
+	//Mouse On Mouse roll up Registry pointer
 	static WRegistry* MOS_MouseRollUp;
+
+	//-->DOC_MEMBER
+	//Mouse On Mouse roll down Registry pointer
 	static WRegistry* MOS_MouseRollDown;
 
+	//-->DOC_MEMBER
+	//Windows on GDI Paint message Registry pointer
 	static WUniqueRegister* WND_OnGDIPaint;
 };
 
