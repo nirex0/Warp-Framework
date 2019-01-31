417375dac0958ffb823a670f25aeab7267c00c81
Nirex0 <Nirex.0@gmail.com>
2019-01-31 12:40:55 +0330

    Update: WEntity is now ready for docreator

--- src/Warp-Framework/WEntity.h
+++ src/Warp-Framework/WEntity.h
@@ -3,6 +3,8 @@
 #ifndef _W_ENTITY_H_
 #define _W_ENTITY_H_
 
+//-->DOC_CLASS
+// Parent of almost all warp-related classes, if you need anything extra just add it to this one
 class WEntity {};
 
 #endif // !_W_ENTITY_H_
