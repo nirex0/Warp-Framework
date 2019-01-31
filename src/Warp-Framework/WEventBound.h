7716699dea2cecca241d6e9fe5bc15766e8aeb5e
Nirex0 <Nirex.0@gmail.com>
2019-01-31 12:40:41 +0330

    Update: WEventBound is now ready for docreator

--- src/Warp-Framework/WEventBound.h
+++ src/Warp-Framework/WEventBound.h
@@ -11,11 +11,13 @@
 
 typedef std::function<void __stdcall (WEntity* sender, WEventArgs* args)> Annex;
 
+//-->DOC_CLASS
+// Binds an Annex to a Bool, if the Annex is successfully completed, the bool will be set to true, it'll be set to false otherwise
 class WEventBound : public WEntity
 {
 public:
-	WEventBound();
-	~WEventBound();
+	WEventBound(void);
+	~WEventBound(void);
 
 	bool Binding(void) const;
 	bool Binding(bool intake);
@@ -23,13 +25,26 @@ public:
 	Annex* Function(void) const;
 	Annex* Function(Annex intake);
 
+	//-->DOC_FUNC
+	// Annex setter operator
 	void operator=(Annex rhs);
+
+	//-->DOC_FUNC
+	// Execution functor to set the bool and run the method
 	void operator()(WEntity* sender, WEventArgs* args);
 
+	//-->DOC_FUNC
+	// Execution method to set the bool and run the method
 	void Run(WEntity* sender, WEventArgs* args);
 
 private:
+	//-->DOC_MEMBER
+	// The boolean of the function
 	bool m_binding;
+
+
+	//-->DOC_MEMBER
+	// The annex of the function
 	Annex* m_annex;
 };
 
