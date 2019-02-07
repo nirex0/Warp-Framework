1315db8927fb49f040636833d102f446e96db574
Nirex0 <Nirex.0@gmail.com>
2019-02-07 22:30:34 +0330

    Update: WRegistry.h is now ready for docreator

--- src/Warp-Framework/WRegistry.h
+++ src/Warp-Framework/WRegistry.h
@@ -11,27 +11,51 @@
 
 typedef std::function<void(WEntity* sender, WEventArgs* args)> Annex;
 
+//-->DOC_CLASS
+// A class for event registry and handling
 class WRegistry final : public WEntity
 {
 public:
-	WRegistry();
+	WRegistry(void);
 	WRegistry(const WRegistry& nRegistery);
 	WRegistry(WRegistry&& nRegistery);
-	~WRegistry();
+	~WRegistry(void);
 	
 	long GetCount(void) const;
 
+	//-->DOC_FUNC
+	// Raise event functor
 	WRegistry* operator()(WEntity* sender, WEventArgs* args);
+
+	//-->DOC_FUNC
+	// Raise event method
 	WRegistry* Run(WEntity* sender, WEventArgs* args);
+
+	//-->DOC_FUNC
+	// Register method
 	WRegistry* Register(Annex rhs);
+
+	//-->DOC_FUNC
+	// Unegister method
 	WRegistry* UnRegister(Annex rhs);
+
+	//-->DOC_FUNC
+	// Register operator
 	WRegistry* operator+=(Annex rhs);
+
+	//-->DOC_FUNC
+	// Ungister operator
 	WRegistry* operator-=(Annex rhs);
 
 	std::vector<Annex*> Container(void) const;
 
 private:
+	//-->DOC_MEMBER
+	// Number of registered annexes
 	volatile long m_count;
+
+	//-->DOC_MEMBER
+	// Vector of registered annexes
 	std::vector<Annex*> m_registered;
 };
 
