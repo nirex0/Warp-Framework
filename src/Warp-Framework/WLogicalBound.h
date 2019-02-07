b9b8491a8d8b532fec04dfe37ce9e548be9f6008
Nirex0 <Nirex.0@gmail.com>
2019-02-07 22:30:10 +0330

    Update: WLogialBound.h is now ready for docreator

--- src/Warp-Framework/WLogicalBound.h
+++ src/Warp-Framework/WLogicalBound.h
@@ -11,25 +11,39 @@
 
 typedef std::function<void(bool&)> WBinding;
 
+//-->DOC_CLASS
+// Binds a WBinding to a Bool, if the WBinding is successfully completed, the bool will be set to true, it'll be set to false otherwise
 class WLogicalBound : public WEntity
 {
 public:
-	WLogicalBound();
-	~WLogicalBound();
+	WLogicalBound(void);
+	~WLogicalBound(void);
 
 	bool Binding(void) const;
 	bool Binding(bool intake);
 
 	WBinding* Function(void) const;
 	WBinding* Function(WBinding intake);
-	
+
+	//-->DOC_FUNC
+	// WBinding setter operator
 	void operator=(WBinding rhs);
+
+	//-->DOC_FUNC
+	// Execution functor to set the bool and run the method
 	void operator()(void);
 
+	//-->DOC_FUNC
+	// Execution method to set the bool and run the method
 	void Run(void);
 
 private:
+	//-->DOC_MEMBER
+	// The boolean of the function
 	bool m_binding;
+	
+	//-->DOC_MEMBER
+	// The WBinding of the function
 	WBinding* m_annex;
 };
 
