1474ded0a52a690c2e242d57b5a4411dbed6c6f8
Nirex0 <Nirex.0@gmail.com>
2019-02-07 22:30:43 +0330

    Update: WUniqueRegister.h is now ready for docreator

--- src/Warp-Framework/WUniqueRegister.h
+++ src/Warp-Framework/WUniqueRegister.h
@@ -11,16 +11,33 @@
 
 typedef std::function<void __stdcall (WEntity* sender, WEventArgs* args)> Annex;
 
+//-->DOC_CLASS
+// A class for event registry and handling (Limited to 1 Annex)
 class WUniqueRegister final : public WEntity
 {
 public:	
-	~WUniqueRegister();
+	WUniqueRegister(void);
+	~WUniqueRegister(void);
+
+	//-->DOC_FUNC
+	// Raise event functor
 	WUniqueRegister* operator()(WEntity* sender, WEventArgs* args);
+
+	//-->DOC_FUNC
+	// Raise event method
 	WUniqueRegister* Run(WEntity* sender, WEventArgs* args);
+
+	//-->DOC_FUNC
+	// Register method
 	WUniqueRegister* Register(Annex intake);
+
+	//-->DOC_FUNC
+	// Unegister method
 	WUniqueRegister* Unregister(void);
 
 private:
+	//-->DOC_MEMBER
+	// Registered annex
 	Annex* m_Register;
 };
 
