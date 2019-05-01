097bb752912cf975ddfec0fbd61c1e806414bdd1
Nirex0 <Nirex.0@gmail.com>
2019-05-01 05:14:51 +0430

    Update: Now All WEntity classes can call Free and free their own memory

--- src/Warp-Framework/WEntity.h
+++ src/Warp-Framework/WEntity.h
@@ -5,6 +5,12 @@
 
 //-->DOC_CLASS
 // Parent of almost all warp-related classes, if you need anything extra just add it to this one
-class WEntity {};
+class WEntity 
+{
+	virtual void Free(void)
+	{
+		delete this;
+	}
+};
 
 #endif // !_W_ENTITY_H_
