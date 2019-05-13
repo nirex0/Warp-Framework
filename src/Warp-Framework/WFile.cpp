2ec3fdb4270c3b035663b5a761a8899d7ad0d41f
Nirex0 <Nirex.0@gmail.com>
2019-05-13 12:43:40 +0430

    Update: WFile class is now static

--- src/Warp-Framework/WFile.cpp
+++ src/Warp-Framework/WFile.cpp
@@ -1,8 +1,9 @@
 //© 2019 NIREX ALL RIGHTS RESERVED
 
 #include "WFile.h"
+#include "WString.h"
 
-std::string WFile::ReadAllText(std::string file)
+std::string WFile::ReadAllText(const std::string& file)
 {
 	std::ifstream f(file.c_str());
 
@@ -19,7 +20,7 @@ std::string WFile::ReadAllText(std::string file)
 	return outstr;
 }
 
-int WFile::WriteAllText(std::string file, std::string str)
+int WFile::WriteAllText(const std::string& file, const std::string& str)
 {
 	std::ofstream f;
 	f.open(file);
@@ -28,7 +29,65 @@ int WFile::WriteAllText(std::string file, std::string str)
 	return 0;
 }
 
-std::int32_t WFile::FindFirst(std::string file, std::string val)
+std::vector<std::string> WFile::ReadAllLines(const std::string& file)
+{
+	std::ifstream f(file.c_str());
+	std::vector<std::string> retv;
+
+	std::string str = "";
+	while (std::getline(f, str))
+	{
+		retv.push_back(str);
+	}
+
+	f.close();
+	return retv;
+}
+
+int WFile::WriteAllLines(const std::string& file, std::vector<std::string> vecstr)
+{
+	std::ofstream f;
+	f.open(file);
+
+	for (size_t i = 0; i < vecstr.size(); i++)
+	{
+		f << vecstr[i];
+		f << std::endl;
+	}
+
+	f.close();
+	return 0;
+}
+
+std::vector<char> WFile::ReadAllBytes(const std::string& fileName)
+{
+	std::ifstream ifs(fileName, std::ios::binary | std::ios::ate);
+	std::ifstream::pos_type pos = ifs.tellg();
+
+	std::vector<char> result((const unsigned int)pos);
+
+	ifs.seekg(0, std::ios::beg);
+	ifs.read(&result[0], pos);
+
+	return result;
+}
+
+int WFile::WriteAllBytes(const std::string& fileName, std::vector<char> vecBytes)
+{
+	std::ofstream ofs(fileName, std::ios::binary | std::ios::out);
+
+	ofs.write(vecBytes.data(), vecBytes.size());
+
+	if (ofs.bad()) //bad() function will check for badbit
+	{
+		return 1;
+	}
+
+	ofs.close();
+	return 0;
+}
+
+std::int32_t WFile::FindFirst(const std::string& file, const std::string& val)
 {
 	std::ifstream f(file.c_str());
 
@@ -46,37 +105,7 @@ std::int32_t WFile::FindFirst(std::string file, std::string val)
 	return -1;
 }
 
-std::vector<std::string> WFile::ReadAllLines(std::string file)
-{
-	std::ifstream f(file.c_str());
-	std::vector<std::string> retv;
-
-	std::string str = "";
-	while (std::getline(f, str))
-	{
-		retv.push_back(str);
-	}
-
-	f.close();
-	return retv;
-}
-
-int WFile::WriteAllLines(std::string file, std::vector<std::string> vecstr)
-{
-	std::ofstream f;
-	f.open(file);
-
-	for (size_t i = 0; i < vecstr.size(); i++)
-	{
-		f << vecstr[i];
-		f << std::endl;
-	}
-
-	f.close();
-	return 0;
-}
-
-std::vector<int32_t> WFile::FindAll(std::string file, std::string val)
+std::vector<int32_t> WFile::FindAll(const std::string& file, const std::string& val)
 {
 	std::ifstream f(file.c_str());
 	std::vector<int32_t> retv;
@@ -95,6 +124,31 @@ std::vector<int32_t> WFile::FindAll(std::string file, std::string val)
 
 	f.close();
 	return retv;
+}
+
+bool WFile::SafeFRead(void* buffer, int size, int number, FILE* fp)
+{
+	using namespace std;
+	int ItemsRead;
+	if (feof(fp))
+	{
+		return false;
+	}
+	ItemsRead = (int)fread(buffer, size, number, fp);
+	if (ItemsRead < number)
+	{
+		return false;
+	}
+	return true;
+}
+
+bool WFile::SafeFWrite(void* buffer, int size, int number, FILE* fp)
+{
+	if (fwrite(buffer, size, number, fp) == number)
+	{
+		return false;
+	}
+	return true;
 }
 
 bool WFile::SafeFread(void * buffer, int size, int number, FILE * fp)
--- src/Warp-Framework/WFile.h
+++ src/Warp-Framework/WFile.h
@@ -3,6 +3,7 @@
 #ifndef _W_FILE_H_
 #define _W_FILE_H_
 
+#include "WDefines.h"
 #include <string>
 #include <fstream>
 #include <vector>
@@ -11,13 +12,20 @@
 class WFile final
 {
 public:
-	std::string ReadAllText(std::string file);
-	int WriteAllText(std::string file, std::string str);
-	std::int32_t FindFirst(std::string file, std::string val);
+	static std::string ReadAllText(const std::string& file);
+	static int WriteAllText(const std::string& file, const std::string& str);
 
-	std::vector<std::string> ReadAllLines(std::string file);
-	int WriteAllLines(std::string file, std::vector<std::string> vecstr);
-	std::vector<int32_t> FindAll(std::string file, std::string val);
+	static std::vector<std::string> ReadAllLines(const std::string& file);
+	static int WriteAllLines(const std::string& file, std::vector<std::string> vecstr);
+
+	static std::vector<char> ReadAllBytes(const std::string& fileName);
+	static int WriteAllBytes(const std::string& fileName, std::vector<char> vecBytes);
+
+	static std::int32_t FindFirst(const std::string& file, const std::string& val);
+	static std::vector<int32_t> FindAll(const std::string& file, const std::string& val);
+
+	static bool SafeFRead(void* buffer, int size, int number, FILE* fp);
+	static bool SafeFWrite(void* buffer, int size, int number, FILE* fp);
 
 	bool SafeFread(void* buffer, int size, int number, FILE* fp);
 };
